973e9f320dbefdfe9c9c8909bbbb86004977defd
Auburngrads <auburngrads@live.com>
2019-04-30 18:52:20 -0400

    initial build

--- R/beta0.2.quan.R
+++ R/beta0.2.quan.R
@@ -1,12 +1,31 @@
 beta0.2.quan <-
-function (ndist1, ndist2, alpha, bds, beta0, beta1, stress, sigma, 
-    mugamma, sdgamma) 
+function (ndist1, 
+          ndist2, 
+          alpha, 
+          bds, 
+          beta0, 
+          beta1, 
+          stress, 
+          sigma,
+          mugamma, 
+          sdgamma) 
 {
     bd1 <- bds[1]
     bd2 <- bds[2]
-    zout <- .Fortran("sbq", as.integer(ndist1), as.integer(ndist2), 
-        as.double(stress), as.double(alpha), as.double(beta0), 
-        as.double(beta1), as.double(sigma), as.double(mugamma), 
-        as.double(sdgamma), as.double(bd1), as.double(bd2), quan = double(1))
+    zout <- SBQ(as.integer(ndist1), 
+                as.integer(ndist2), 
+                as.double(stress), 
+                as.double(alpha), 
+                as.double(beta0), 
+                as.double(beta1), 
+                as.double(sigma), 
+                as.double(mugamma), 
+                as.double(sdgamma), 
+                as.double(bd1), 
+                as.double(bd2), 
+                quan = double(1),
+                as.integer(kprint))
+    
     return(zout$quan)
+    
 }
