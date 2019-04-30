2eb40d391e5b4c25d59710d0ef3c8d7b1874ef91
Auburngrads <auburngrads@live.com>
2019-04-30 18:51:30 -0400

    initial build

--- R/NHPP.MCFvec.R
+++ R/NHPP.MCFvec.R
@@ -1,8 +1,14 @@
 NHPP.MCFvec <-
-function (times, theta, form)
+function (times, 
+          theta, 
+          form)
 {
-    zout <- .Fortran("sfmcf", time = as.double(times), kform = as.integer(num.nhpp.form(generic.nhpp.form(form)[[1]])),
-        theta = as.double(theta), ntimes = as.integer(length(times)),
-        answer = double(length(times)))
+    zout <- SFMCF(time = as.double(times), 
+                  kform = as.integer(num.nhpp.form(generic.nhpp.form(form)[[1]])),
+                  theta = as.double(theta), 
+                  ntimes = as.integer(length(times)),
+                  answer = double(length(times)))
+    
     return(zout$answer)
+    
 }
