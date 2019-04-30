a52a3ffa63803f6a18d507a766660d0e44e470c1
Auburngrads <auburngrads@live.com>
2019-04-30 18:52:50 -0400

    initial build

--- R/cprobur.R
+++ R/cprobur.R
@@ -1,11 +1,29 @@
 cprobur <-
-function (tvec, r1log, mut2, sigmat2, mur1, sigmar1, mur2, sigmar2, 
-    rho) 
+function (tvec, 
+          r1log, 
+          mut2, 
+          sigmat2, 
+          mur1, 
+          sigmar1, 
+          mur2, 
+          sigmar2,
+          rho) 
 {
-    zout <- .Fortran("ssft2gr1", tvec = as.double(tvec), nt = as.integer(length(tvec)), 
-        mut1 = as.double(r1log), mut2 = as.double(mut2), sigmat2 = as.double(sigmat2), 
-        mur1 = as.double(mur1), sigmar1 = as.double(sigmar1), 
-        mur2 = as.double(mur2), sigmar2 = as.double(sigmar2), 
-        rho = as.double(rho), answer = double(length(tvec)))
+    
+    zout <- SSFT2GR1(t = as.double(tvec), 
+                     nt = as.integer(length(tvec)), 
+                     as.double(r1log), 
+                     mut2 = as.double(mut2), 
+                     sigmat2 = as.double(sigmat2), 
+                     mur1 = as.double(mur1), 
+                     sigmar1 = as.double(sigmar1), 
+                     mur2 = as.double(mur2), 
+                     sigmar2 = as.double(sigmar2), 
+                     rho = as.double(rho), 
+                     answer = double(length(tvec)),
+                     ier = integer(1),
+                     kprint = kprint)
+    
     return(zout$answer)
+    
 }
