82e8ac1c065e05e114cde266946172b3412b349d
Auburngrads <auburngrads@live.com>
2019-04-30 18:54:52 -0400

    initial build

--- R/prediction.order.average.R
+++ R/prediction.order.average.R
@@ -1,9 +1,23 @@
 prediction.order.average <-
-function (korder, nsamsize, timevec, mu, sigma, distribution) 
+function (korder, 
+          nsamsize, 
+          timevec, 
+          mu, 
+          sigma, 
+          distribution) 
 {
-    zout <- .Fortran("postkp", as.integer(korder), as.integer(nsamsize), 
-        as.double(timevec), as.integer(length(timevec)), as.double(mu), 
-        as.double(sigma), as.integer(length(mu)), as.integer(numdist(distribution)), 
-        pdf = double(length(timevec)), cdf = double(length(timevec)))
-    return(list(x = timevec, post.pred.den = zout$pdf, post.pred.cdf = zout$cdf))
+    zout <- POSTKP(as.integer(korder), 
+                   as.integer(nsamsize), 
+                   as.double(timevec),
+                   as.integer(length(timevec)), 
+                   as.double(mu), 
+                   as.double(sigma),
+                   as.integer(length(mu)), 
+                   as.integer(numdist(distribution)), 
+                   pdf = double(length(timevec)), 
+                   cdf = double(length(timevec)))
+    
+    return(list(x = timevec, 
+                post.pred.den = zout$pdf, 
+                post.pred.cdf = zout$cdf))
 }
