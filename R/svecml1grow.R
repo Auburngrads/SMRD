6e209e285b8840e3af2bc49af7b58d4d878b5c46
Auburngrads <auburngrads@live.com>
2019-04-30 18:55:25 -0400

    initial build

--- R/svecml1grow.R
+++ R/svecml1grow.R
@@ -1,10 +1,20 @@
 svecml1grow <-
-function (times, a2.init, a2.limit, rate, rate.factor) 
+function (times, 
+          a2.init, 
+          a2.limit, 
+          rate, 
+          rate.factor) 
 {
     number.times <- length(times)
     rate.factor <- expand.vec(rate.factor, number.times)
-    zout <- .Fortran("mlmod1", as.double(times), as.integer(number.times), 
-        as.double(a2.init), as.double(a2.limit), as.double(rate), 
-        as.double(rate.factor), a2 = double(number.times))
+    
+    zout <- MLMOD1(as.double(times), 
+                   as.integer(number.times), 
+                   as.double(a2.init), 
+                   as.double(a2.limit),
+                   as.double(rate), 
+                   as.double(rate.factor),
+                   a2 = double(number.times))
+    
     return(zout$a2)
 }
