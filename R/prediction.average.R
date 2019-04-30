d1d1b764a77cd9498d871d03418d192403bcd60f
Auburngrads <auburngrads@live.com>
2019-04-30 18:54:46 -0400

    initial build

--- R/prediction.average.R
+++ R/prediction.average.R
@@ -1,9 +1,19 @@
 prediction.average <-
-function (timevec, mu, sigma, distribution) 
+function (timevec, 
+          mu, 
+          sigma, 
+          distribution) 
 {
-    zout <- .Fortran("postpr", as.double(timevec), as.integer(length(timevec)), 
-        as.double(mu), as.double(sigma), as.integer(length(mu)), 
-        as.integer(numdist(distribution)), pdf = double(length(timevec)), 
-        cdf = double(length(timevec)))
-    return(list(x = timevec, post.pred.den = zout$pdf, post.pred.cdf = zout$cdf))
+    zout <- POSTPR(as.double(timevec), 
+                   as.integer(length(timevec)), 
+                   as.double(mu), 
+                   as.double(sigma),
+                   as.integer(length(mu)),
+                   as.integer(numdist(distribution)), 
+                   pdf = double(length(timevec)), 
+                   cdf = double(length(timevec)))
+    
+    return(list(x = timevec, 
+                post.pred.den = zout$pdf, 
+                post.pred.cdf = zout$cdf))
 }
