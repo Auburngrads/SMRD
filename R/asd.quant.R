49110853d5238c349bdc29983677f43d6cad62ce
Auburngrads <auburngrads@live.com>
2019-04-30 18:52:14 -0400

    initial build

--- R/asd.quant.R
+++ R/asd.quant.R
@@ -1,15 +1,27 @@
 asd.quant <-
-function (plan.values, n, censor.time, quantile.mark) 
+function (plan.values, 
+          n, 
+          censor.time, 
+          quantile.mark) 
 {
     distribution <- basic.distribution(plan.values$distribution)
-    if (any(quantile.mark <= 0) || any(quantile.mark >= 1)) 
+    
+    if (any(quantile.mark <= 0) || any(quantile.mark >= 1)) {
+        
         stop("Specified quanttile not between 0-1.")
-    zc <- (logb(censor.time) - plan.values$mu)/plan.values$sigma
+        
+    }
+    zc <- (logb(censor.time) - plan.values$mu) / plan.values$sigma
     ze <- quant(quantile.mark, plan.values$distribution)
     vlength <- max(length(zc), length(ze))
     zc <- expand.vec(zc, vlength)
     ze <- expand.vec(ze, vlength)
-    zout <- .Fortran("vavar", as.integer(numdist(distribution)), 
-        as.integer(vlength), as.double(zc), as.double(ze), ans = double(vlength))
-    return(sqrt(zout$ans/n) * plan.values$sigma)
+    
+    zout <- VAVAR(as.integer(idist),
+                  as.integer(vlength),
+                  as.double(zc),
+                  as.double(ze),
+                  double(vlength))
+    
+    return(sqrt(zout$avar / n) * plan.values$sigma)
 }
