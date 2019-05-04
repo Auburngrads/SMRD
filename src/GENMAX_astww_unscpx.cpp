003e3c7221c34acc08da4b4425c75fbee7e220d4
Auburngrads <auburngrads@live.com>
2019-05-04 08:39:09 -0400

    still working GENMAX -- fixed comment lines

--- src/GENMAX_astww_unscpx.cpp
+++ src/GENMAX_astww_unscpx.cpp
@@ -11,6 +11,14 @@ void unscpx(Rcpp::NumericVector &thetas,
    
 int ipoint;
 Rcpp::IntegerVector ipoinx;
+Rcpp::IntegerVector NXG = clone(genx20::g_nxg);
+Rcpp::IntegerVector INTG = clone(genx20::g_intg);
+Rcpp::NumericVector IPXBRU = clone(genx05::g_ipxbru);
+Rcpp::NumericVector IPSD = clone(genx05::g_ipsd);
+Rcpp::IntegerVector IPISCD = clone(genx05::g_ipiscd);
+Rcpp::List IPXCG = clone(genx20::g_ipxcg);
+Rcpp::IntegerVector IPTHET = clone(genx21::g_ipthet);
+Rcpp::IntegerVector NTERG = clone(genx20::g_nterg);
    
 if(debug::kprint >= 6){
    
@@ -21,17 +29,17 @@ if(debug::kprint >= 6){
 for(int igame = 1; igame <= genx03::g_ngame; igame++){
    
     // Get pointer to current theta vector
-       ipoint = genx21::ipthet.at(igame - 1);
+       ipoint = IPTHET.at(igame - 1);
    
     // Check to see if we are dealing with a trivial parameter that needs no scaling:
     // nterg(igame)=0 indicates that this is a saved function of another parmeter
-       if(genx20::nterg.at(igame - 1) !=  0) {
+       if(NTERG.at(igame - 1) !=  0) {
        
           // nxg(igame)=0   indicates no regression--copy single
-             if(genx20::nxg.at(igame - 1) != 0) {
+             if(NXG.at(igame - 1) != 0) {
              
                 // Get pointer to the current x columns
-                   SEXP l = genx20::ipxcg[igame - 1]; Rcpp::IntegerVector ipoinx(l);
+                   SEXP l = IPXCG[igame - 1]; Rcpp::IntegerVector ipoinx(l);
                    
                    if(debug::kprint >= 3){
                       
@@ -39,22 +47,9 @@ for(int igame = 1; igame <= genx03::g_ngame; igame++){
                       
                    }
                    
-                   Rcpp::IntegerVector NXG = clone(genx20::nxg);
-                   Rcpp::IntegerVector INTG = clone(genx20::intg);
-                   Rcpp::NumericVector IPXBRU = clone(genx05::g_ipxbru);
-                   Rcpp::NumericVector IPSD = clone(genx05::g_ipsd);
-                   Rcpp::IntegerVector IPISCD = clone(genx05::g_ipiscd);
-                   
                    unsclp(ipoint,thetas,NXG.at(igame - 1),
                           INTG.at(igame - 1),ipoinx,
                           IPXBRU,IPSD,theta,IPISCD);
-                   
-                   genx20::nxg = clone(NXG);
-                   genx20::intg = clone(INTG);
-                   genx05::g_ipxbru = clone(IPXBRU);
-                   genx05::g_ipsd = clone(IPSD);
-                   genx05::g_ipiscd = clone(IPISCD);
-
                    
                    goto line21;
             
@@ -67,14 +62,23 @@ for(int igame = 1; igame <= genx03::g_ngame; igame++){
       line21: if(debug::kprint >= 6){
          
                  Rcpp::Rcout << "\nigame = " << igame - 1 << std::endl;
-                 Rcpp::Rcout << "ipthet(igame) = " << ipthet.at(igame - 1) << std::endl;
-                 Rcpp::Rcout << "intg(igame) = " << intg.at(igame - 1) << std::endl;
-                 Rcpp::Rcout << "nxg(igame) = " << nxg.at(igame - 1) << std::endl;
-                 Rcpp::Rcout << "nterg(igame) = " << nterg.at(igame - 1) << std::endl;
+                 Rcpp::Rcout << "ipthet(igame) = " << IPTHET.at(igame - 1) << std::endl;
+                 Rcpp::Rcout << "intg(igame) = " << INTG.at(igame - 1) << std::endl;
+                 Rcpp::Rcout << "nxg(igame) = " << NXG.at(igame - 1) << std::endl;
+                 Rcpp::Rcout << "nterg(igame) = " << NTERG.at(igame - 1) << std::endl;
                  
       }
       
 }
+
+genx20::g_nxg = clone(NXG);
+genx20::g_intg = clone(INTG);
+genx05::g_ipxbru = clone(IPXBRU);
+genx05::g_ipsd = clone(IPSD);
+genx05::g_ipiscd = clone(IPISCD);
+genx20::g_ipxcg = clone(IPXCG);
+genx21::g_ipthet = clone(IPTHET);
+genx20::g_nterg = clone(NTERG);
 
 if(debug::kprint >= 6){
    
