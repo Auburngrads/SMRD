e613c76c0080a02e10cfc3d7afc06e90229187f3
Auburngrads <auburngrads@live.com>
2019-05-04 11:39:47 -0400

    updated cpp_tests/ to ensure all calls are to 'SMRD2' and removed all 'SMRD:::' calls in R/

--- inst/cpp_tests/POSTPR_test.R
+++ inst/cpp_tests/POSTPR_test.R
@@ -1,4 +1,4 @@
-library(smrdfortran)
+library(SMRD)
 timevec = rexp(10)
 mu = 0
 sigma = 1
@@ -6,12 +6,12 @@ distribution = 'weibull' # issues with exponential
 
 old <- .Fortran("postpr", as.double(timevec), as.integer(length(timevec)), 
                 as.double(mu), as.double(sigma), as.integer(length(mu)), 
-                as.integer(smrdfortran:::numdist(distribution)), pdf = double(length(timevec)), 
+                as.integer(SMRD:::numdist(distribution)), pdf = double(length(timevec)), 
                 cdf = double(length(timevec)))
 
 new <- SMRD2::POSTPR(as.double(timevec), as.integer(length(timevec)), 
                         as.double(mu), as.double(sigma), as.integer(length(mu)), 
-                        as.integer(smrdfortran:::numdist(distribution)), pdf = double(length(timevec)), 
+                        as.integer(SMRD:::numdist(distribution)), pdf = double(length(timevec)), 
                         cdf = double(length(timevec)))
 
 old$cdf - new$cdf
