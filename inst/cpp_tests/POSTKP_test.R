b5bb7d093a3523b38ad659b6102e34737c303e31
Auburngrads <auburngrads@live.com>
2019-05-04 11:39:44 -0400

    updated cpp_tests/ to ensure all calls are to 'SMRD2' and removed all 'SMRD:::' calls in R/

--- inst/cpp_tests/POSTKP_test.R
+++ inst/cpp_tests/POSTKP_test.R
@@ -1,4 +1,4 @@
-library(smrdfortran)
+library(SMRD)
 korder = 2
 nsamsize = 10
 timevec = 5
@@ -8,12 +8,12 @@ distribution = 'lognormal'
 
 old <- .Fortran("postkp", as.integer(korder), as.integer(nsamsize), 
                 as.double(timevec), as.integer(length(timevec)), as.double(mu), 
-                as.double(sigma), as.integer(length(mu)), as.integer(smrdfortran:::numdist(distribution)), 
+                as.double(sigma), as.integer(length(mu)), as.integer(SMRD:::numdist(distribution)), 
                 pdf = double(length(timevec)), cdf = double(length(timevec)))
 
-new <- wqmmlesss::postkp(as.integer(korder), as.integer(nsamsize), 
+new <- SMRD2::postkp(as.integer(korder), as.integer(nsamsize), 
                         as.double(timevec), as.integer(length(timevec)), as.double(mu), 
-                        as.double(sigma), as.integer(length(mu)), as.integer(smrdfortran:::numdist(distribution)), 
+                        as.double(sigma), as.integer(length(mu)), as.integer(SMRD:::numdist(distribution)), 
                         pdf = double(length(timevec)), cdf = double(length(timevec)))
 
 old$pdf - new$pdf
