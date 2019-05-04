f4621db2b812ad9cc698f5f5c9b9827693a36468
Auburngrads <auburngrads@live.com>
2019-05-04 11:40:24 -0400

    updated cpp_tests/ to ensure all calls are to 'SMRD2' and removed all 'SMRD:::' calls in R/

--- inst/cpp_tests/SXPDF3_test.R
+++ inst/cpp_tests/SXPDF3_test.R
@@ -1,4 +1,4 @@
-library(smrdfortran)
+library(SMRD)
 ndist1 = 2
 ndist2 = 2
 beta0 = 30.27241
@@ -13,13 +13,13 @@ kprint = 2
     
       max.length <- max(length(beta0), length(beta1), length(sigma),
                         length(ugamma), length(sgamma), length(stress), length(w))
-      beta0  <- smrdfortran:::expand.vec(beta0, max.length)
-      beta1  <- smrdfortran:::expand.vec(beta1, max.length)
-      sigma  <- smrdfortran:::expand.vec(sigma, max.length)
-      ugamma <- smrdfortran:::expand.vec(ugamma, max.length)
-      sgamma <- smrdfortran:::expand.vec(sgamma, max.length)
-      stress <- smrdfortran:::expand.vec(stress, max.length)
-      w      <- smrdfortran:::expand.vec(w, max.length)
+      beta0  <- SMRD:::expand.vec(beta0, max.length)
+      beta1  <- SMRD:::expand.vec(beta1, max.length)
+      sigma  <- SMRD:::expand.vec(sigma, max.length)
+      ugamma <- SMRD:::expand.vec(ugamma, max.length)
+      sgamma <- SMRD:::expand.vec(sgamma, max.length)
+      stress <- SMRD:::expand.vec(stress, max.length)
+      w      <- SMRD:::expand.vec(w, max.length)
       if (debug1) browser()
       zout <- .Fortran("sxpdf3", as.integer(ndist1), as.integer(ndist2),
                        as.double(beta0), as.double(beta1), as.double(stress),
@@ -27,7 +27,7 @@ kprint = 2
                        as.double(w), as.integer(max.length), answer = double(max.length),
                        ier = integer(max.length))
       
-      new = wqmmlesss::sxpdf3(as.integer(ndist1), as.integer(ndist2),
+      new = SMRD2::sxpdf3(as.integer(ndist1), as.integer(ndist2),
                        as.double(beta0), as.double(beta1), as.double(stress),
                        as.double(sigma), as.double(ugamma), as.double(sgamma),
                        as.double(w), as.integer(max.length), answer = double(max.length),
