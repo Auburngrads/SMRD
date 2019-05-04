d4c19f1e2aa5f978834566ac17a855b68a0e8a8d
Auburngrads <auburngrads@live.com>
2019-05-04 08:39:33 -0400

    still working GENMAX -- fixed comment lines

--- src/GENMAX_fitww_genest_mlrsd_rgamme.cpp
+++ src/GENMAX_fitww_genest_mlrsd_rgamme.cpp
@@ -22,18 +22,27 @@ void rgamme(int &kpnow,
             Rcpp::NumericVector &gamme){
 
 Rcpp::IntegerVector xcols;
+Rcpp::IntegerVector NXG = clone(genx20::g_nxg);
+Rcpp::IntegerVector INTG = clone(genx20::g_intg);
+Rcpp::IntegerVector NTERG = clone(genx20::g_nterg);
+Rcpp::IntegerVector IGTYG = clone(genx21::g_igtyg);
+Rcpp::IntegerVector IPTHET = clone(genx21::g_ipthet);
+Rcpp::NumericVector IPSD = clone(genx05::g_ipsd);
+Rcpp::IntegerVector IRELAG = clone(genx21::g_irelag);
+Rcpp::NumericVector GAMMS = clone(genx21::g_gamms);
+
 // Iterate over all parameters and functions of parameters
    for(int igam = 1; igam <= genx03::g_ngame; igam++){
       
        // If the parameter does not depend on any explanatory
        // variables, skip and get saved value from gamms
-          if(genx20::nxg.at(igam - 1) != 0) {
+          if(genx20::g_nxg.at(igam - 1) != 0) {
           
              // If the 'parameter' is a direct function, it is already in place so skip
-                if(genx20::nterg.at(igam - 1) == 0) continue;
+                if(genx20::g_nterg.at(igam - 1) == 0) continue;
                 
              // Otherwise take relationship info below to get the values from exp vars
-                SEXP x = genx20::ipxcg[igam - 1];
+                SEXP x = genx20::g_ipxcg[igam - 1];
                 Rcpp::IntegerVector xcols(x);
                 
                 if(debug::kprint >= 5){
@@ -41,52 +50,46 @@ Rcpp::IntegerVector xcols;
                    Rcpp::Rcout << "\nRGAMME BEFORE RGAMI\n" << std::endl;
                    Rcpp::Rcout << "kpnow = " << kpnow << std::endl;
                    Rcpp::Rcout << "igam = " << igam << std::endl;
-                   Rcpp::Rcout << "nxg.at(igam) = " << genx20::nxg.at(igam - 1) << std::endl;
-                   Rcpp::Rcout << "nterg.at(igam) = " << genx20::nterg.at(igam - 1) << std::endl;
-                   Rcpp::Rcout << "intg.at(igam) = " << genx20::intg.at(igam - 1) << std::endl;
+                   Rcpp::Rcout << "nxg(igam) = " << NXG.at(igam - 1) << std::endl;
+                   Rcpp::Rcout << "nterg(igam) = " << NTERG.at(igam - 1) << std::endl;
+                   Rcpp::Rcout << "intg(igam) = " << INTG.at(igam - 1) << std::endl;
                    Rcpp::Rcout << "xcols = " << xcols << std::endl;
-                   Rcpp::Rcout << "ipthet.at(igam) = " << genx21::ipthet.at(igam - 1) << std::endl;
-                   Rcpp::Rcout << "igtyg.at(igam) = " << genx21::igtyg.at(igam - 1) << std::endl;
-                   Rcpp::Rcout << "irelag.at(igam) = " << genx21::irelag.at(igam - 1) << std::endl;
+                   Rcpp::Rcout << "ipthet(igam) = " << IPTHET.at(igam - 1) << std::endl;
+                   Rcpp::Rcout << "igtyg(igam) = " << IGTYG.at(igam - 1) << std::endl;
+                   Rcpp::Rcout << "irelag(igam) = " << IRELAG.at(igam - 1) << std::endl;
                    Rcpp::Rcout << "thetas = " << thetas << std::endl;
                    Rcpp::Rcout << "gamme = " << gamme << std::endl;
                    
                 }
                 
-                Rcpp::IntegerVector NXG = clone(genx20::nxg);
-                Rcpp::IntegerVector INTG = clone(genx20::intg);
-                Rcpp::IntegerVector NTERG = clone(genx20::nterg);
-                Rcpp::IntegerVector IGTYG = clone(genx21::igtyg);
-                Rcpp::IntegerVector IPTHET = clone(genx21::ipthet);
-                Rcpp::NumericVector IPSD = clone(genx05::g_ipsd);
-                Rcpp::IntegerVector IRELAG = clone(genx21::irelag);
-
                 rgami(kpnow,igam,NXG.at(igam - 1),NTERG.at(igam - 1),
                       INTG.at(igam - 1),xcols,IPTHET.at(igam - 1),
                       IGTYG.at(igam - 1),IRELAG.at(igam - 1),thetas,gamme);
-                
-                genx20::nxg = clone(NXG);
-                genx20::intg = clone(INTG);
-                genx20::nterg = clone(NTERG);
-                genx21::igtyg = clone(IGTYG);
-                genx21::ipthet = clone(IPTHET);
-                genx05::g_ipsd = clone(IPSD);
-                genx21::irelag = clone(IRELAG);
                 
                 continue;
           
           }
           
-          gamme.at(igam - 1) = genx21::gamms.at(igam - 1);
+          gamme.at(igam - 1) = genx21::g_gamms.at(igam - 1);
          
    }
+   
+   genx20::g_nxg = clone(NXG);
+   genx20::g_intg = clone(INTG);
+   genx20::g_nterg = clone(NTERG);
+   genx21::g_igtyg = clone(IGTYG);
+   genx21::g_ipthet = clone(IPTHET);
+   genx05::g_ipsd = clone(IPSD);
+   genx21::g_irelag = clone(IRELAG);
+   genx21::g_gamms = clone(GAMMS);
+
    
    if(debug::kprint >= 7){
       
       Rcpp::Rcout << "\nRGAMME**7**\n"   << std::endl;
       Rcpp::Rcout << "kpnow = " << kpnow << std::endl;
       Rcpp::Rcout << "gamme = " << gamme << std::endl;
-      
+      Rcpp::Rcout << "thetas = " << thetas << std::endl;
       
    }
    
