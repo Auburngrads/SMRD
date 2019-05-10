280299b950256296ef6bfe47ad2630b57c15703f
Auburngrads <auburngrads@live.com>
2019-05-10 16:22:19 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

---
+++ inst/include/mlsim2/rbino.hpp
@@ -0,0 +1,9 @@
+#ifndef SMRD_rbino_H
+#define SMRD_rbino_H
+void rbino(int &numsim,
+           double &pp,
+           int &n,
+           Rcpp::NumericVector &tspass,
+           bool &lrand,
+           Rcpp::IntegerVector &ivec);
+#endif
