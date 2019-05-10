36aef3bf3a30450515d9e0caecd3993c00d4be96
Auburngrads <auburngrads@live.com>
2019-05-10 16:19:43 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

--- src/WQMCDFEST_wqm_cdfest_wqm_cdfesi_wqm_invpx.cpp
+++ src/WQMCDFEST_wqm_cdfest_wqm_cdfesi_wqm_invpx.cpp
@@ -12,94 +12,109 @@ void wqm_invpx(Rcpp::NumericVector &f,
                int &n,
                int &ier){
 
-double tol  = 0.0625e00;
-double x,test;
-double relz;
+double tol = 0.0625e00;
+double x,test,sum,relz;
+int ipndex = 1,iphold,kount;
+int ipind1 = 1,nm1,jndex,index,jindex;
+int jl,indlow,jm1,jndlow;
+
 relz = tol / n;
 ier = 0;
-int ipndex = 1,iphold,kount;
-int ipind1 = 0,nm1,jndex,index,sum,jindex;
-int jl,indlow,jm1,jndlow;
 
 for(int i = 1; i <= n; i++){
 
-   iphold = ipndex;
-   kount = 1;
+    iphold = ipndex;
+    kount = 1;
    
-   for(int j = 1; j <= i; j++){
+    for(int j = 1; j <= i; j++){
 
-       x = f.at(ipndex - 1);
+        x = f.at(ipndex - 1);
 
-       if(j != 1) {
+        if(j != 1) {
 
-          for(int k = iphold; k <= ipind1; k++) {
+           for(int k = iphold; k <= ipind1; k++) {
 
-              x = x - f.at(k - 1) * f.at(kount - 1);
-              kount = kount + 1;
+               x = x - f.at(k - 1) * f.at(kount - 1);
+               kount = kount + 1;
 
-          }
+           }
           
-       }
-       
-       if(i == j) {
-
-          test = f.at(ipndex - 1) + x * relz;
-
-          if(test <= f.at(ipndex - 1)) { ier = 1; return; }
-
-          f.at(ipndex - 1) = one / std::sqrt(x);
-
-        } else {
-
-          f.at(ipndex - 1) = x * f.at(kount - 1);
-
         }
+       
+        if(i == j) {
+
+           test = f.at(ipndex - 1) + x * relz;
+
+           if(test <= f.at(ipndex - 1)) { ier = 1; return; }
+
+           f.at(ipndex - 1) = one / std::sqrt(x);
+
+         } else {
+
+           f.at(ipndex - 1) = x * f.at(kount - 1);
+
+         }
         
-        ipind1 = ipndex;
-        ipndex = ipndex + 1;
-        kount  = kount + 1;
+         ipind1 = ipndex;
+         ipndex = ipndex + 1;
+         kount  = kount + 1;
    
    }
    
+}
+
+if(debug::kprint >= 5) {
+    
+   Rcpp::Rcout << "\nWQM_INVPX AFTER LOOP1\n" << std::endl;
+   Rcpp::Rcout << "f = " << f << std::endl;
+   Rcpp::Rcout << "n = " << n << std::endl;
+    
 }
 
 nm1 = n - 1;
 
-if(n != 1) {
+if(n == 1) goto line55;
 
-   index = 1;
+index = 1;
 
-   for(int i = 1; i <= nm1; i++){
-
-       ipind1 = i + 1;
-       jm1 = i;
-       jndex = index;
-
-       for(int j = ipind1; j <= n; j++){
-
-           sum = zero;
-           indlow = index;
-           jindex = jndex + i;
-           jl = jindex;
-
-           for(int l = i; l <= jm1; l++){
-              
-               sum = sum + f.at(indlow - 1) * f.at(jl - 1);
-               jl = jl + 1;
-               indlow = indlow + l;
-               
-           }
-
-           jndex = jndex + j;
-           f.at(jindex - 1) = -1 * f.at(jndex - 1) * sum;
-           jm1 = j;
-
-       }
-
-       index = index + ipind1;
-
-   }
+for(int i = 1; i <= nm1; i++){
+    
+    ipind1 = i + 1;
+    jm1 = i;
+    jndex = index;
+    
+    for(int j = ipind1; j <= n; j++){
+        
+        sum = zero;
+        indlow = index;
+        jindex = jndex + i;
+        jl = jindex;
+     
+        for(int l = i; l <= jm1; l++){
+     
+            sum = sum + f.at(indlow - 1) * f.at(jl - 1);
+            jl = jl + 1;
+            indlow = indlow + l;
+      
+        }
+     
+        jndex = jndex + j;
+        f.at(jindex - 1) = -1.0e00 * f.at(jndex - 1) * sum;
+        jm1 = j;
+     
+    }
+    
+    index = index + ipind1;
+    
 }
+
+line55: if(debug::kprint >= 5) {
+    
+           Rcpp::Rcout << "\nWQM_INVPX AFTER LOOP2\n" << std::endl;
+           Rcpp::Rcout << "f = " << f << std::endl;
+           Rcpp::Rcout << "n = " << n << std::endl;
+            
+        }
 
 index = 0;
 
@@ -115,7 +130,7 @@ for(int i = 1; i <= n; i++){
         jndex = jndex + j;
         jndlow = jndex;
 
-        for(int l = j; j <= n; l++){
+        for(int l = j; l <= n; l++){
            
             sum = sum + f.at(indlow - 1) * f.at(jndlow - 1);
             indlow = indlow + l;
@@ -129,6 +144,14 @@ for(int i = 1; i <= n; i++){
     index = index + i;
     
 }
+
+if(debug::kprint >= 5) {
+    
+   Rcpp::Rcout << "\nWQM_INVPX AFTER LOOP3\n" << std::endl;
+   Rcpp::Rcout << "f = " << f << std::endl;
+    
+}
+
 
 return;
 
