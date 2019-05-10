a066d8127b3836cfedc20cfcdbc0316121298c5a
Auburngrads <auburngrads@live.com>
2019-05-10 16:22:11 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

---
+++ inst/include/mlsim2/qbino.hpp
@@ -0,0 +1,8 @@
+#ifndef SMRD_qbino_H
+#define SMRD_qbino_H
+void qbino(Rcpp::NumericVector &pquan, 
+           Rcpp::IntegerVector &n, 
+           Rcpp::NumericVector &pbin, 
+           int &number, 
+           Rcpp::IntegerVector &kqb);
+#endif
