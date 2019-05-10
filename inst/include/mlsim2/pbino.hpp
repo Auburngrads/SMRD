497cb8d16393ae37853bdc1380385a8f3d85a27d
Auburngrads <auburngrads@live.com>
2019-05-10 16:22:02 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

---
+++ inst/include/mlsim2/pbino.hpp
@@ -0,0 +1,8 @@
+#ifndef SMRD_pbino_H
+#define SMRD_pbino_H
+void pbino(Rcpp::IntegerVector &ix,
+           Rcpp::IntegerVector &n,
+           Rcpp::NumericVector &p,
+           Rcpp::NumericVector &ans,
+           int &number);
+#endif
