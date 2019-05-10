4069d14117842d1b113e4dc7340f549ba859c81c
Auburngrads <auburngrads@live.com>
2019-05-10 15:48:12 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

--- inst/include/wqm_cdfest/wqm_cdfzs.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef smrd_wqm_cdfzs_H
-#define smrd_wqm_cdfzs_H
-void wqm_cdfzs(int &m,
-               int &ier,
-               Rcpp::NumericVector &sd,
-               int &nnzs,
-               int &maxmsd);
-#endif
