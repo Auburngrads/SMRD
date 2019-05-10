eee7566ef727779837f1420130fcb00cea0c93ce
Auburngrads <auburngrads@live.com>
2019-05-10 15:45:18 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

--- R/get.npbands.R
+++ R/get.npbands.R
@@ -1,15 +1,25 @@
 get.npbands <-
-function (cdfest.out, band.type, how.show.interval = "step.fun", 
-    conf.level = GetSMRDDefault("SMRD.ConfLevel")/100, a.limit = 0.001, 
-    b.limit = 0.999) 
+function (cdfest.out,
+          band.type,
+          how.show.interval = "step.fun",
+          conf.level = GetSMRDDefault("SMRD.ConfLevel")/100,
+          a.limit = 0.001,
+          b.limit = 0.999) 
 {
-    switch(how.show.interval, sf = , step.fun = {
-        bands <- get.step.bands(cdfest.out, band.type = band.type, 
-            conf.level = conf.level, a.limit = a.limit, b.limit = b.limit)
-    }, pwlf = , pw.linear.fun = {
-        bands <- get.pw.linear.bands(cdfest.out, band.type = band.type, 
-            conf.level = conf.level, a.limit = a.limit, b.limit = b.limit)
-    }, stop(paste("how.show.interval variable not recognized \n", 
-        "Must use step.fun (sf) or pw.linear.fun (pwlf)")))
-    return(bands)
+    switch(how.show.interval, 
+           sf = , step.fun = { bands <- get.step.bands(cdfest.out, 
+                                                      band.type = band.type, 
+                                                      conf.level = conf.level, 
+                                                      a.limit = a.limit, 
+                                                      b.limit = b.limit) }, 
+           pwlf = , pw.linear.fun = { bands <- get.pw.linear.bands(cdfest.out,
+                                                                   band.type = band.type,
+                                                                   conf.level = conf.level,
+                                                                   a.limit = a.limit,
+                                                                   b.limit = b.limit) }, 
+           stop(paste("how.show.interval variable not recognized \n", 
+                      "Must use step.fun (sf) or pw.linear.fun (pwlf)")))
+    
+return(bands)
+    
 }
