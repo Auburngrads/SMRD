78bb7a956f2c9e976c7cf7f45ac92cc11974c2be
Auburngrads <auburngrads@live.com>
2019-05-10 16:22:14 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

---
+++ inst/include/mlsim2/qpois.hpp
@@ -0,0 +1,7 @@
+#ifndef SMRD_qpois_H
+#define SMRD_qpois_H
+void qpois(Rcpp::NumericVector &pquan, 
+           Rcpp::NumericVector &xlam, 
+           int &number, 
+           Rcpp::IntegerVector &kqp);
+#endif
