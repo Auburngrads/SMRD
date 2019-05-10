f657d5eef2e932c69644d95afff325b73c6e623d
Auburngrads <auburngrads@live.com>
2019-05-10 16:19:58 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

--- src/WQMMLESSS_wqm_mlboth_wqm_gtdev.cpp
+++ src/WQMMLESSS_wqm_mlboth_wqm_gtdev.cpp
@@ -5,8 +5,6 @@
 #include <wqmsphiall/wqm_ztran.hpp>
 #include <wqmmlesss/wqm_fliket.hpp>
 
-using namespace lstd;
-
 //' Compute the deviance
 
 void wqm_gtdev(Rcpp::NumericMatrix &y,
@@ -39,14 +37,12 @@ if(debug::kprint >= 1) {
   
 }
 
-lstd::g_ltp = true;
-
 double epslike = 1.0e-04, addon;
 Rcpp::NumericVector qder(5);
 
-double sigma, sigmal, epsxxx, z, xmu;
-double trl, tru, z2, flikem;
-int itype, ittype;
+double sigma, sigmal, epsxxx, z = 0.0e00, xmu;
+double trl, tru, z2 = 0.0e00, flikem;
+int itype, ittype = 1;
 flikes  = -1.0e15;
 
 // This is different from wqm_flike where we have to exponentiate
@@ -54,9 +50,9 @@ flikes  = -1.0e15;
    sigmal = std::log(sigma);
    flikes = 0.0e00;
 
-for(int i = 0; i < nrow; i++){
+for(int i = 1; i <= nrow; i++){
   
-    itype = cen.at(i);
+    itype = cen.at(i - 1);
     if(itype == 0) continue;
     epsxxx = zero;
 
@@ -71,28 +67,37 @@ for(int i = 0; i < nrow; i++){
     // distributions with addon is actually -log(t-gamthr)
 
     addon = zero;
-    if((leven) and (itype == 1)) addon = -1 * y.at(i,0);
+    if((leven) and (itype == 1)) addon = -1 * y.at(i - 1,0);
     xmu = wqm_dfxmu(i,xnew,nrow,nter,thetg,
                     nparm,upcen,sigma);
-    z = (y.at(i,0) - xmu - epsxxx) / sigma;
+    z = (y.at(i - 1,0) - xmu - epsxxx) / sigma;
     z = wqm_ztran(z,kdist);
     
     if(itype >= 4){
       
-       z2 = (y.at(i,ny - 1) - xmu + epsxxx) / sigma;
+       z2 = (y.at(i - 1,ny - 1) - xmu + epsxxx) / sigma;
        z2 = wqm_ztran(z2, kdist);
        
     }
     
+    if(debug::kprint >= 6){
+      
+       Rcpp::Rcout << "\nGTDEV**6**\n" << std::endl;
+       Rcpp::Rcout << "cen = " << itype << std::endl;
+       Rcpp::Rcout << "z = "  << z  << std::endl;
+       Rcpp::Rcout << "z2 = " << z2 << std::endl;
+      
+    }
+    
     if(nty > 0){
       
-       trl = (ty.at(i,0) - xmu) / sigma;
+       trl = (ty.at(i - 1,0) - xmu) / sigma;
        trl = wqm_ztran(trl,kdist);
-       ittype = tcodes.at(i);
+       ittype = tcodes.at(i - 1);
        
        if(ittype == 4){
          
-          tru = (ty.at(i,1) - xmu) / sigma;
+          tru = (ty.at(i - 1,1) - xmu) / sigma;
           tru = wqm_ztran(tru, kdist);
          
        }
@@ -104,25 +109,30 @@ for(int i = 0; i < nrow; i++){
 	  wqm_derivt(qder,itype,z,z2,nty,
                ittype,trl,tru,kdist);
 	  
-		devian.at(i,1) = qder.at(0) * wt.at(i);
-		devian.at(i,2) = qder.at(2) * wt.at(i);
-    flikes = flikes + flikem * wt.at(i);
-    devian.at(i,0) =  flikem * wt.at(i);
+		devian.at(i - 1,1) = qder.at(0) * wt.at(i - 1);
+		devian.at(i - 1,2) = qder.at(2) * wt.at(i - 1);
+    flikes = flikes + flikem * wt.at(i - 1);
+    devian.at(i - 1,0) =  flikem * wt.at(i - 1);
     
     if(debug::kprint >= 4){
       
-       Rcpp::Rcout << "\nGTDEV**4**\n" << std::endl;
-       Rcpp::Rcout << "i" << i << std::endl;
-       Rcpp::Rcout << "ny" << ny << std::endl;
-       Rcpp::Rcout << "devian(i,0)" << devian(i,0) << std::endl;
-       Rcpp::Rcout << "flikes" << flikes << std::endl;
-       Rcpp::Rcout << "xnew(i,0)" << xnew(i,0) << std::endl;
-       Rcpp::Rcout << "xnew(i,nter)" << xnew(i,nter - 1) << std::endl;
-       Rcpp::Rcout << "y(i,0)" << y(i,0) << std::endl;
-       Rcpp::Rcout << "y(i,ny)" << y(i,ny - 1) << std::endl;
-       Rcpp::Rcout << "xmu" << xmu << std::endl;
-       Rcpp::Rcout << "sigma" << sigma << std::endl;
-       Rcpp::Rcout << "z" << z << std::endl;
+       Rcpp::NumericVector devrowi = devian.row(i - 1);
+       Rcpp::Rcout << "\nGTDEV**4**\n"           << std::endl;
+       Rcpp::Rcout << "           i = " << i - 1 << std::endl;
+       Rcpp::Rcout << "          ny = " << ny << std::endl;
+       Rcpp::Rcout << "       itype = " << itype << std::endl;
+       Rcpp::Rcout << "      ittype = " << ittype << std::endl;
+       Rcpp::Rcout << "  devian(i,) = " << devrowi << std::endl;
+       Rcpp::Rcout << "        qder = " << qder << std::endl;
+       Rcpp::Rcout << "       wt(i) = " << wt.at(i - 1) << std::endl;
+       Rcpp::Rcout << "      flikes = " << flikes << std::endl;
+       Rcpp::Rcout << "   xnew(i,0) = " << xnew.at(i - 1,0) << std::endl;
+       Rcpp::Rcout << "xnew(i,nter) = " << xnew.at(i - 1,nter - 1) << std::endl;
+       Rcpp::Rcout << "      y(i,0) = " << y.at(i - 1,0) << std::endl;
+       Rcpp::Rcout << "     y(i,ny) = " << y.at(i - 1,ny - 1) << std::endl;
+       Rcpp::Rcout << "         xmu = " << xmu << std::endl;
+       Rcpp::Rcout << "       sigma = " << sigma << std::endl;
+       Rcpp::Rcout << "           z = " << z << std::endl;
       
     }
            
@@ -136,12 +146,12 @@ if(kprint >= 4) {
    thetb.at(0) = thetb.at(0) - upcen * sigma;
    
    Rcpp::Rcout << "\nGTDEV**end**\n" << std::endl;
-   Rcpp::Rcout << "flikes" << flikes << std::endl;
-   Rcpp::Rcout << "thetb" << thetb << std::endl;
-   Rcpp::Rcout << "sigma" << sigma << std::endl;
+   Rcpp::Rcout << "flikes = " << flikes << std::endl;
+   Rcpp::Rcout << "thetb = " << thetb << std::endl;
+   Rcpp::Rcout << "sigma = " << sigma << std::endl;
 
 }
 
-  return;
+return;
 
 }
