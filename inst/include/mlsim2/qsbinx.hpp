a613b5cda273c7352503d46141f49c0768fa1149
Auburngrads <auburngrads@live.com>
2019-05-10 16:22:17 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

---
+++ inst/include/mlsim2/qsbinx.hpp
@@ -0,0 +1,9 @@
+#ifndef SMRD_qsbinx_H
+#define SMRD_qsbinx_H
+void qsbinx(int &kmeth,
+            double pquan,
+            Rcpp::IntegerVector &numrem,
+            Rcpp::NumericVector &rhostr,
+            int &ngroup,
+            int &kans);
+#endif
