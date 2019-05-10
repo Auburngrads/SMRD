cd0c162db37ae07543a723dfed0a85fa34856a7d
Auburngrads <auburngrads@live.com>
2019-05-10 15:45:51 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

--- R/linax.R
+++ R/linax.R
@@ -6,7 +6,8 @@ function (xmax, xmin, nint = 5, nticks = GetSMRDDefault("SMRD.NumberTicks"),...)
         xmax <- xmax[2]
     }
     ticlab <- wqm.pretty(c(xmax, xmin), nint = nint)
-    ticloc <- seq(ticlab[1], ticlab[length(ticlab)], length = (length(ticlab) -
-        1) * (nticks + 1) + 1)
+    ticloc <- seq(ticlab[1], ticlab[length(ticlab)], 
+                  length = (length(ticlab) - 1) * (nticks + 1) + 1)
+    
     return(list(ticlab = format(ticlab), ticloc = format(ticloc)))
 }
