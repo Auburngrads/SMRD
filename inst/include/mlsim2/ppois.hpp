6d60c55efaeb1cfbeb09fa6242ac552f8422a295
Auburngrads <auburngrads@live.com>
2019-05-10 16:22:06 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

---
+++ inst/include/mlsim2/ppois.hpp
@@ -0,0 +1,7 @@
+#ifndef SMRD_ppois_H
+#define SMRD_ppois_H
+void ppois(Rcpp::IntegerVector &ix,
+           Rcpp::NumericVector &xlamb,
+           Rcpp::NumericVector &ans,
+           int& number);
+#endif
