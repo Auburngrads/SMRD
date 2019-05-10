dc72d403b881ea03adc47c508ecb797278e98135
Auburngrads <auburngrads@live.com>
2019-05-10 15:46:34 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

--- R/quant.R
+++ R/quant.R
@@ -1,6 +1,14 @@
 quant <-
 function (p, distribution) 
 {
+  
+    for(i in seq_along(p)){
+            
+        if((p[i] < 0) & (abs(p[i] - 0) < 1e-6)) p[i] <- 0
+        if((p[i] > 1) & (abs(p[i] - 1) < 1e-6)) p[i] <- 1
+            
+    }
+  
     if (any(p < 0) | any(p > 1)) {
       
         stop(paste("\nquantiles must be between 0 and 1", p))
