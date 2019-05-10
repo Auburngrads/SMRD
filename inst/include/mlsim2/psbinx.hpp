3376c828821e9e799abb350fb8f5bcc66203cf9c
Auburngrads <auburngrads@live.com>
2019-05-10 16:22:09 -0400

    lots of updates beginning to add documentation - cleanup cpp & vignettes

---
+++ inst/include/mlsim2/psbinx.hpp
@@ -0,0 +1,9 @@
+#ifndef SMRD_psbinx_H
+#define SMRD_psbinx_H
+void psbinx(int &kmeth,
+            int &kfv,
+            Rcpp::IntegerVector &numrem,
+            Rcpp::NumericVector &rhostr,
+            int &ngroup,
+            double &ans);
+#endif
