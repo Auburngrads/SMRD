63a1b0fc98a0b2b647b289e20814957fdf5d4cd8
Auburngrads <auburngrads@live.com>
2019-05-11 13:41:14 -0400

    update through vignettes through echapter3

--- R/plot.life.data.R
+++ R/plot.life.data.R
@@ -2,26 +2,44 @@
 plot.life.data <-
 function (x, distribution = NULL, ...)
 {
-    if (!is.null(distribution)) {
-      
-      if (length(distribution)==4) {
-        tmp <- four.npprobplot(data.ld = x, distribution.list = distribution,...)
-      }
-      
-      if (length(distribution)==2) {
-        tmp <- two.npprobplot(data.ld = x, distribution.list = distribution,...)
-      }
-      
-      if (length(distribution)==1) {
-        tmp <- npprobplot(data.ld = x, distribution = distribution,...)
-      }
-      
-      if (length(distribution)==3 | length(distribution) > 4) {
-        stop('The number of distributions must be either 1, 2, or 4')
-      }
-      
   
-    } else { tmp <- cdfplot(data.ld = x, ...)  }
+# if distribution is NA, NULL, or nchar(distribution) == 0
+  if(missing(distribution) | nnnc0(distribution)) { 
+      
+     tmp <- cdfplot(data.ld = x, ...) 
+     return(invisible(tmp))
+       
+  }
   
-    invisible(tmp)
+# If a single distribution is given
+  if(length(distribution) == 1) {
+      
+     tmp <- npprobplot(data.ld = x, distribution = distribution,...)
+     return(invisible(tmp))
+        
+  }
+    
+# If a vector of distributions are given
+  if(length(distribution) > 1) {
+    
+     rows = floor(length(distribution) / 2) +  length(distribution) %% 2
+
+     matc = integer(rows * 2)
+
+     for(i in 1:length(distribution)) matc[i] = matc[i] + i
+     
+     layout(mat = matrix(matc, nrow = rows, ncol = cols, byrow = T))
+
+     for(i in 1:length(distribution)){
+       
+        tmp <- npprobplot(data.ld = data.ld, 
+                          distribution = distribution[i],
+                          my.title = paste0(toupper(distribution[i])),...)
+       
+     }      
+  
+     return(invisible(tmp))
+     
+  }
+     
 }
