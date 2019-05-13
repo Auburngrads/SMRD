986c2a63fa2a81bee3d81e0e58b6244d95cfc611
Auburngrads <auburngrads@live.com>
2019-05-13 17:13:46 -0400

    lots of changes: documentation, vignettes, inst, warning & stop statements in cpp, etc.

--- R/AF.R
+++ R/AF.R
@@ -1,3 +1,27 @@
+#' Acceleration factors
+#'
+#' @param stress 
+#' @param stress0 
+#' @param coef 
+#' @param relationship 
+#' @param power 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' AF(160,80,.8,"arrhenius")
+#' 
+#' AFplot(160,80,.8,"arrhenius")
+#' 
+#' AFplot(160,80,c(.7,.8,.9),"arrhenius")
+#' 
+#' AF(130,110,-2,"Inverse Power Rule")
+#' 
+#' AFplot(130,110,-2,"Inverse Power Rule")
+#' }
 AF <-
 function (stress, stress0, coef, relationship, power)
 {
