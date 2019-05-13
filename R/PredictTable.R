6c5d5a73db9585f1c9844911a0ddc907c694f0a2
Auburngrads <auburngrads@live.com>
2019-05-13 17:14:14 -0400

    lots of changes: documentation, vignettes, inst, warning & stop statements in cpp, etc.

--- R/PredictTable.R
+++ R/PredictTable.R
@@ -1,3 +1,51 @@
+#' Title
+#'
+#' @param mlest.out 
+#' @param FtimeStart 
+#' @param FtimeEnd 
+#' @param warranty.time 
+#' @param print.total 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' DeviceN.ld <- frame.to.ld(devicen,
+#'                           response.column = "months", 
+#'                           censor.column = "event",
+#'                           case.weight.column = "counts")
+#'                           
+#' PredictTable(DeviceN.mlest.lnorm,
+#'              FtimeStart = 0,
+#'              FtimeEnd = 1)
+#' 
+#' PredictTable(DeviceN.mlest.lnorm,
+#'              FtimeStart = 1,
+#'              FtimeEnd = 2)
+#' 
+#' PredictTable(DeviceN.mlest.lnorm,
+#'              FtimeStart = 0,
+#'              FtimeEnd = 36)
+#' 
+#' PredictTable(DeviceN.mlest.lnorm,
+#'              FtimeStart = 0,
+#'              FtimeEnd = 36,
+#'              warranty.time = 36)
+#' 
+#' DeviceN.mlest.weib <- mlest(DeviceN.ld,
+#'                             dist = "Weibull")
+#' 
+#' PredictTable(DeviceN.mlest.weib,
+#'              FtimeStart = 1,
+#'              FtimeEnd = 2)
+#' 
+#' PredictTable(DeviceN.mlest.weib,
+#'              FtimeStart = 0,
+#'              FtimeEnd = 36)
+#' 
+#' }
 PredictTable <-
 function (mlest.out, FtimeStart, FtimeEnd, warranty.time = Inf, 
     print.total = TRUE) 
