da3d764fb19bea7ef3ae758587d15c07a3799306
Auburngrads <auburngrads@live.com>
2019-05-13 17:16:00 -0400

    lots of changes: documentation, vignettes, inst, warning & stop statements in cpp, etc.

--- R/get.alt.test.plan.direct.R
+++ R/get.alt.test.plan.direct.R
@@ -1,3 +1,56 @@
+#' Title
+#'
+#' @param accel.variable.levels 
+#' @param number.of.units 
+#' @param censor.times 
+#' @param accelvar.names 
+#' @param describe.string 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' advbond.model1 <- 
+#'   get.alt.plan.values.from.two.points(
+#'     distribution = "Weibull",
+#'     relationship = "Inverse Power Rule", 
+#'     time.units = "hours", 
+#'     censor.time = 1000,
+#'     probs = c(.001,.9), 
+#'     accelvar.units = "volts", 
+#'     accelvar = c(110,150), 
+#'     beta = 1.667)
+#' 
+#' advbond.test.plan1 <- 
+#'   get.alt.test.plan.direct(accel.variable.levels = c(130,140,150),
+#'                            number.of.units = c(100,100,100))
+#' 
+#' plot(advbond.test.plan1,
+#'      ALT.plan.values = advbond.model1, 
+#'      my.title = "",
+#'      use.conditions = 120)
+#' 
+#' advbond.model2 <- 
+#'   get.alt.plan.values.from.two.points(distribution = "Lognormal",	
+#'                                       relationship = "Arrhenius", 
+#'                                       time.units = "days",
+#'                                       censor.time = 183,
+#'                                       probs = c(.001,.9),
+#'                                       accelvar = c(50,120),
+#'                                       sigma = .5)
+#' 
+#' 
+#' advbond.test.plan2 <- get.alt.test.plan.direct(accel.variable.levels = c(80,100,120),
+#'                          number.of.units = c(100,100,100))
+#' 
+#' plot(advbond.test.plan2, 
+#'      ALT.plan.values = advbond.model2, 
+#'      my.title = "",
+#'      use.condition = 50)
+#' }
+
 get.alt.test.plan.direct <-
 function (accel.variable.levels, number.of.units, censor.times = NULL, 
     accelvar.names = "accel.variable", describe.string = "") 
