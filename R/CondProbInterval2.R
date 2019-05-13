49d641f779ce412fd3c0a3ef14c721d7f43d8370
Auburngrads <auburngrads@live.com>
2019-05-13 17:13:53 -0400

    lots of changes: documentation, vignettes, inst, warning & stop statements in cpp, etc.

--- R/CondProbInterval2.R
+++ R/CondProbInterval2.R
@@ -1,3 +1,40 @@
+#' Title
+#'
+#' @param mlest.out 
+#' @param age 
+#' @param tL 
+#' @param tU 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' DeviceN.ld <- frame.to.ld(devicen,
+#'                           response.column = "months", 
+#'                           censor.column = "event",
+#'                           case.weight.column = "counts")
+#' DeviceN.mlest.lnorm <- mlest(DeviceN.ld,
+#'                              dist = "Lognormal")
+#' 
+#' CondProbInterval2(DeviceN.mlest.lnorm,
+#'                   age = 1,
+#'                   tL = 1,
+#'                   tU = 36)
+#' CondProbInterval2(DeviceN.mlest.lnorm,
+#'                   age = 2,
+#'                   tL = 2,
+#'                   tU = 36)
+#' CondProbInterval2(DeviceN.mlest.lnorm,
+#'                   age = 1,
+#'                   tL = 1,
+#'                   tU = 2)
+#' CondProbInterval2(DeviceN.mlest.lnorm,
+#'                   age = 2,
+#'                   tL = 2,
+#'                   tU = 3)
+#' }
 CondProbInterval2 <-
 function (mlest.out, age, tL, tU) 
 {
