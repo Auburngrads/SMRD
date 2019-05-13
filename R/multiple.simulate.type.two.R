50a9f8ee0ecee7b3960341dbdd11ceba38e203aa
Auburngrads <auburngrads@live.com>
2019-05-13 17:18:15 -0400

    lots of changes: documentation, vignettes, inst, warning & stop statements in cpp, etc.

--- R/multiple.simulate.type.two.R
+++ R/multiple.simulate.type.two.R
@@ -1,3 +1,42 @@
+#' Title
+#'
+#' @param n 
+#' @param r 
+#' @param distribution 
+#' @param number.sim 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' mstt1 <- multiple.simulate.type.two(n = c(20,30,40), 
+#'                                     r = c(20,30,40),
+#'                                     distribution = "lognormal",
+#'                                     number.sim = 1000)
+#' 
+#' plot(mstt1, qprob = 0.9)
+#' plot(mstt1, qprob = 0.8)
+#' plot(mstt1, qprob = 0.5)
+#' 
+#' 
+#' mstt2 <- multiple.simulate.type.two(n = c(20,30,40), 
+#'                                     r = c(10,15,20),
+#'                                     distribution = "lognormal",
+#'                                     number.sim = 1000)
+#' 
+#' plot(mstt2, qprob = 0.9)
+#' plot(mstt2, qprob = 0.8)
+#' plot(mstt2, qprob = 0.5, grids = T)
+#' 
+#' mstt3 <- multiple.simulate.type.two(n = c(24,28,33), 
+#'                                     r = c(24,28,33),
+#'                                     distribution = "normal", 
+#'                                     number.sim = 1000)
+#' 
+#' plot(mstt3, qprob = 0.9)
+#' }
 multiple.simulate.type.two <-
 function (n, r, distribution, number.sim = 1000)
 {
