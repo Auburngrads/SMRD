50529019119c30616959fea0fc90fd57b5770365
Auburngrads <auburngrads@live.com>
2019-05-15 16:34:35 -0400

    finished exporting functions in echapter vignettes

--- R/renewal.plots.R
+++ R/renewal.plots.R
@@ -1,3 +1,24 @@
+#' Title
+#'
+#' @param data.rdu 
+#' @param my.title 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' halfbeak.rdu <- frame.to.rdu(halfbeak,
+#'                              ID.column = "unit", 
+#'                              time.column = "hours" ,
+#'                              event.column = "event", 
+#'                              data.title = "Halfbeak Data", 
+#'                              time.units = "Thousands of Hours of Operation")
+#' 
+#' renewal.plots(halfbeak.rdu)
+#' 
+#' }
 renewal.plots <-
 function (data.rdu, my.title = NULL) 
 {
