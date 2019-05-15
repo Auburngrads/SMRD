7125655a17392f9a139a0d98ee0fa74126efb01f
Auburngrads <auburngrads@live.com>
2019-05-15 16:30:53 -0400

    finished exporting functions in echapter vignettes

--- R/g.R
+++ R/g.R
@@ -1,3 +1,69 @@
+#' Title
+#'
+#' @param x 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' ZelenCap.ld <- frame.to.ld(zelencap,
+#'                            response.column = 1,
+#'                            censor.column = 2,
+#'                            case.weight.column = 3,
+#'                            x.columns = c(4, 5),
+#'                            time.units = "Hours")
+#' 
+#' ZelenCap.groupm.out2 <-
+#'   groupm.mleprobplot(ZelenCap.ld,
+#'                      distribution = "Lognormal",
+#'                      formula= Location ~ + g(celsius),
+#'                      relationship = c("arrhenius", "log"))
+#' 
+#' 
+#' ## Fitting to `volts` only
+#' 
+#' ZelenCap.groupm.out3 <-
+#'   groupm.mleprobplot(ZelenCap.ld,
+#'                      distribution = "Lognormal",
+#'                      formula= Location ~ + g(volts),
+#'                      relationship = c("arrhenius", "log"))
+#' 
+#' ## Fitting to `celsius` and `volts`
+#' 
+#' ZelenCap.groupm.out4 <-
+#'   groupm.mleprobplot(ZelenCap.ld,
+#'                      distribution = "normal",
+#'                      formula = Location ~ + g(celsius) +  g(volts))
+#' 
+#' ZelenCap.groupm.out5 <-
+#'   groupm.mleprobplot(ZelenCap.ld,
+#'                      distribution = "normal",
+#'                      formula= Location ~  +  g(volts) + g(celsius))
+#' 
+#' ## Fitting to `volts` and `celsius` with interaction
+#' 
+#' ZelenCap.groupm.out6 <-
+#'   groupm.mleprobplot(ZelenCap.ld,
+#'                      distribution = "normal",
+#'                      formula = Location ~ celsius + volts + celsius:volts )
+#' 
+#' ## Omit the formula, use default of linear on all X's
+#' 
+#' ZelenCap.groupm.out7 <-
+#'   groupm.mleprobplot(ZelenCap.ld,
+#'                      distribution = "Lognormal",
+#'                      relationship=c("arrhenius","log"))
+#' 
+#' ZelenCap.groupm.out8 <- 
+#'   groupm.mleprobplot(ZelenCap.ld, 
+#'                      distribution = "Lognormal", 
+#'                      relationship = c("linear", "linear"), 
+#'                      formula = Location ~ + g(volts) + g(celsius) + g(volts):g(celsius))
+#' 
+#' 
+#' }
 g <-
 function (x)
 {
