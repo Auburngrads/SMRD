7ce6abfb274c0f65b8e7f1dbfe35f5f621d3b536
Auburngrads <auburngrads@live.com>
2019-05-14 23:04:13 -0400

    fixing vignettes

---
+++ inst/test_int.cpp
@@ -0,0 +1,29 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+int test_int(int x) {
+  
+  int OUT = x + (x - 1) / 2;
+  return OUT;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+test_int(42)
+*/
