ec74eaf23869480ef2f174e1c39606118930ae1d
Auburngrads <auburngrads@live.com>
2019-05-15 16:34:15 -0400

    finished exporting functions in echapter vignettes

--- R/mcf.plot.R
+++ R/mcf.plot.R
@@ -1,3 +1,42 @@
+#' Title
+#'
+#' @param data.rdu 
+#' @param conf.level 
+#' @param plot.data 
+#' @param title.option 
+#' @param my.title 
+#' @param xlab 
+#' @param ylab 
+#' @param add 
+#' @param lty 
+#' @param plot.seg 
+#' @param xlim 
+#' @param ylim 
+#' @param band.type 
+#' @param ci.opt 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' ValveSeat.rdu <- frame.to.rdu(valveseat,
+#'                               ID.column = "unit", 
+#'                               time.column = "days" ,
+#'                               event.column = "event", 
+#'                               data.title = "Valve-Seat Replacement Data",
+#'                               time.units = "Days")
+#' 
+#' #attr(ValveSeat.rdu,"WindowInfo")
+#' 
+#' summary(ValveSeat.rdu)
+#' 
+#' event.plot(ValveSeat.rdu)
+#' 
+#' mcf.plot(ValveSeat.rdu)
+#' 
+#' }
 mcf.plot <-
 function (data.rdu, conf.level = GetSMRDDefault("SMRD.ConfLevel")/100, 
     plot.data = F, title.option = GetSMRDDefault("SMRD.TitleOption"), my.title = NULL, xlab = paste("Time in", 
