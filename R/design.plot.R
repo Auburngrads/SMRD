d60de28c64ee6a26c96cd80a1203fbe4c22ba1fb
Auburngrads <auburngrads@live.com>
2019-05-15 16:28:54 -0400

    finished exporting functions in echapter vignettes

--- R/design.plot.R
+++ R/design.plot.R
@@ -1,3 +1,32 @@
+#' Title
+#'
+#' @param data.ld 
+#' @param x.cols 
+#' @param xlab 
+#' @param ylab 
+#' @param pch 
+#' @param cex 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples 
+#' \dontrun{
+#' 
+#' Tantalum.ld <- frame.to.ld(tantalum, 
+#'                            response.column = 1,
+#'                            censor.column = 2, 
+#'                            case.weight.column = 3, 
+#'                            x.column = c(4, 5), 
+#'                            data.title = "Tantalum Capacitor Data", 
+#'                            time.units = "Hours", 
+#'                            xlabel = c("Volts","DegreesC"))
+#' 
+#' summary(Tantalum.ld)
+#' 
+#' design.plot(Tantalum.ld)
+#' 
+#' }
 design.plot <-
 function (data.ld, x.cols = c(1, 2), xlab = get.x.columns(data.ld)[x.cols[1]], 
     ylab = get.x.columns(data.ld)[x.cols[2]], pch = 16, cex = 1.5) 
