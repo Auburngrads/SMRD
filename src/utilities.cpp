67af0b7939da49e2c958f16631ee971fc06a0744
Auburngrads <auburngrads@live.com>
2019-05-18 13:18:49 -0400

    lots of fixes

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -64,22 +64,22 @@ Rcpp::IntegerVector merge_sortd(Rcpp::NumericVector x,
 
   if(iorder.at(n - 1) - iorder.at(0) < 0.0001) {
 
-     for(int i = 0; i < n; i++){
+     for(int i = 1; i <= n; i++){
 
-         iorder.at(i) = i;
+         iorder.at(i - 1) = i;
 
      }
   }
 
-    for(int j = 0; j < n; j++){
+    for(int j = 1; j <= n; j++){
 
-        tempmat.row(j) = clone(x);
+        tempmat.row(j - 1) = clone(x);
 
     }
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
 
-      rowi = tempmat.row(i);
+      rowi = tempmat.row(i - 1);
       tmin = which_min(rowi);
       tempvec.at(tmin) = i;
       tempmat.column(tmin) = MAXV;
@@ -88,18 +88,18 @@ Rcpp::IntegerVector merge_sortd(Rcpp::NumericVector x,
 
     // second round
 
-    for(int j = 0; j < n; j++){
+    for(int j = 1; j <= n; j++){
 
-        tempmat2.row(j) = clone(tempvec);
+        tempmat2.row(j - 1) = clone(tempvec);
 
      }
 
 
-  for(int i = 0; i < n; i++) {
+  for(int i = 1; i <= n; i++) {
 
-      tempvec2 = tempmat2.row(i);
+      tempvec2 = tempmat2.row(i - 1);
       tmin = which_min(tempvec2);
-      key.at(i) = iorder.at(tmin);
+      key.at(i - 1) = iorder.at(tmin);
       tempmat2.column(tmin) = MAXV;
 
     }
@@ -131,23 +131,23 @@ Rcpp::IntegerVector merge_sorti(Rcpp::IntegerVector x,
 
   if(iorder.at(n - 1) - iorder.at(0) < 0.0001) {
 
-     for(int i = 0; i < n; i++){
+     for(int i = 1; i <= n; i++){
 
-         iorder.at(i) = i;
+         iorder.at(i - 1) = i;
 
      }
 
   }
 
-    for(int j = 0; j < n; j++){
+    for(int j = 1; j <= n; j++){
 
-        tempmat.row(j) = clone(x);
+        tempmat.row(j - 1) = clone(x);
 
      }
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
 
-        rowi = tempmat.row(i);
+        rowi = tempmat.row(i - 1);
         tmin = which_min(rowi);
         tempvec.at(tmin) = i;
         tempmat.column(tmin) = MAXV;
@@ -157,18 +157,18 @@ Rcpp::IntegerVector merge_sorti(Rcpp::IntegerVector x,
 
     // second round
 
-    for(int j = 0; j < n; j++){
+    for(int j = 1; j <= n; j++){
 
-        tempmat2.row(j) = clone(tempvec);
+        tempmat2.row(j - 1) = clone(tempvec);
 
      }
 
 
-  for(int i = 0; i < n; i++) {
+  for(int i = 1; i <= n; i++) {
 
-      tempvec2 = tempmat2.row(i);
+      tempvec2 = tempmat2.row(i - 1);
       tmin = which_min(tempvec2);
-      key.at(i) = iorder.at(tmin);
+      key.at(i - 1) = iorder.at(tmin);
       tempmat2.column(tmin) = MAXV;
 
     }
@@ -377,7 +377,6 @@ return;
 //'            1     if(yd(k1).gt.yd(k2))
 //'            0     if(yd(k1).eq.yd(k2))
 //'           -1     if(yd(k1).lt.yd(k2))
-
 int icompd(int k1,
            int k2,
            Rcpp::NumericVector yd){
@@ -387,6 +386,23 @@ int icompd(int k1,
 
       return 0;
 }
+
+//'  Integer function to compare two elements of a vector
+//'  
+//'  returns:
+//'            1     if(yd(k1).gt.yd(k2))
+//'            0     if(yd(k1).eq.yd(k2))
+//'           -1     if(yd(k1).lt.yd(k2))
+int icompi(int k1,
+           int k2,
+           Rcpp::IntegerVector yd){
+
+      if((yd.at(k1 - 1) - yd.at(k2 - 1)) < 0) return -1;
+      if((yd.at(k1 - 1) - yd.at(k2 - 1)) > 0) return  1;
+
+      return 0;
+}
+
 
 //' wqm_sortxx will perform a quick sort and will return key(n),;
 //' the ordering vector of indices for y(n). y is not changed.;
@@ -665,5 +681,353 @@ for(int i = 1; i <= n; i++){
 }
 
 return v_min;
+
+}
+
+
+#include <base/base.hpp>
+#include <utility/icompd.hpp>
+
+// uses xkey to make an order-index array iorder, using merge-sort;
+// generalized to allow sorting based on an arbitrary comparison function;
+// and adapted from michel olagnon's mrgref in orderpac;
+// merge sort is a stable sorting algorithm;
+
+void merge_sortdd(Rcpp::NumericVector &xkey, 
+                  int nvalp, 
+                  Rcpp::IntegerVector &iorder, 
+                  Rcpp::IntegerVector &iscrat){
+
+int lmtna, lmtnc;
+int nval, iind, iwrkd, iwrk, iwrkf, jinda, iinda, iindb;
+int kdump = 1;
+
+nval = std::abs(nvalp);
+
+if (nval == 0) return;
+
+// fill-in the dummy index array if nvalp > 0;
+// otherwise move around the iorder sent down;
+// # (useful for ordering more than one vector);
+   for(int i = 1; i <= nval; i++){
+     
+       if(nvalp > 0) {
+         
+          iscrat.at(i - 1) = i;
+         
+        } else {
+         
+          iscrat.at(i - 1) = iorder.at(i - 1);
+       
+       }
+
+   }
+
+   if(debug::kprint > 7){
+     
+     Rcpp::Rcout << "\nsortdd initial iscrat check\n" << std::endl;
+     Rcpp::Rcout << "iscrat = " << iscrat << std::endl;
+     
+   }
+// create ordered couples;
+   for(int iind = 2; iind <= nval; iind = iind + 2){
+     
+       if (icompd(iscrat.at(iind - 2), iscrat.at(iind - 1), xkey) <= 0) {
+         
+           iorder.at(iind - 2) = iscrat.at(iind - 2);
+           iorder.at(iind - 1) = iscrat.at(iind - 1);
+       
+         } else {
+         
+           iorder.at(iind - 2) = iscrat.at(iind - 1);
+           iorder.at(iind - 1) = iscrat.at(iind - 2);
+       
+       }
+
+   }
+
+if ((nval % 2) != 0) iorder.at(nval - 1) = iscrat.at(nval - 1);
+
+// we will now have ordered subsets a - b - a - b - ...;
+// and merge a and b couples into c - c - ...;
+   lmtnc = 2;
+   lmtna = 2;
+
+// iteration. each time, the length of the ordered subsets is doubled.;
+
+while(true){
+  
+  if (lmtna >= nval) break;
+  iwrkf = 0;
+  lmtnc = 2 * lmtnc;
+  iwrk = 0;
+
+// loop on merges of a and b into c;
+   while(true){
+     
+     iinda = iwrkf;
+     iwrkd = iwrkf + 1;
+     iwrkf = iinda + lmtnc;
+     jinda = iinda + lmtna;
+     if(iwrkf >= nval) {
+       
+        if(jinda >= nval) break;
+        iwrkf = nval;
+        
+     }
+     iindb = jinda;
+
+// shortcut for the case when the max of a is smaller;
+// than the min of b (no need to do anything);
+   if(icompd(iorder.at(jinda - 1),iorder.at(jinda),xkey) <= 0) {
+  
+      iwrk = iwrkf;
+      continue;
+      
+   }
+
+// one steps in the c subset, that we create in the final rank array;
+   while(true){
+  
+   if(iwrk >= iwrkf) {
+   
+   // make a copy of the rank array for next iteration;
+   // iorder (iwrkd:iwrkf) = iscrat (iwrkd:iwrkf);
+      for(int i = iwrkd; i <= iwrkf; i++){
+        
+          iorder.at(i - 1) = iscrat.at(i - 1);
+        
+      }
+      break;
+   
+   }
+
+iwrk = iwrk + 1;
+
+// we still have unprocessed values in both a and b;
+if(iinda < jinda) {
+
+   if(iindb < iwrkf) {
+
+      if(icompd(iorder.at(iinda),iorder.at(iindb),xkey) > 0) {
+        
+         iindb = iindb + 1;
+         iscrat.at(iwrk - 1) = iorder.at(iindb - 1);
+         
+      } else {
+        
+         iinda = iinda + 1;
+         iscrat.at(iwrk - 1) = iorder.at(iinda - 1);
+      
+      }
+    
+   } else {
+
+      // only a still has unprocessed values;
+         iinda = iinda + 1;
+         iscrat.at(iwrk - 1) = iorder.at(iinda - 1);
+      
+   }
+
+} else {
+
+  // only b still has unprocessed values;
+  // iorder (iwrkd:iindb) = iscrat (iwrkd:iindb);
+      for(int i = iwrkd; i <= iindb; i++){
+        
+          iorder.at(i - 1) = iscrat.at(i - 1);
+        
+      }
+  
+     iwrk = iwrkf;
+     break;
+
+}
+
+}
+
+}
+
+// the cs become as and bs;
+   lmtna = 2 * lmtna;
+
+}
+
+return;
+
+}
+
+
+#include <base/base.hpp>
+#include <utility/icompi.hpp>
+
+// uses xkey to make an order-index array iorder, using merge-sort;
+// generalized to allow sorting based on an arbitrary comparison function;
+// and adapted from michel olagnon's mrgref in orderpac;
+// merge sort is a stable sorting algorithm;
+
+void merge_sortii(Rcpp::IntegerVector &xkey, 
+                 int nvalp, 
+                 Rcpp::IntegerVector &iorder, 
+                 Rcpp::IntegerVector &iscrat){
+
+int lmtna, lmtnc;
+int nval, iind, iwrkd, iwrk, iwrkf, jinda, iinda, iindb;
+int kdump = 1;
+
+nval = std::abs(nvalp);
+
+if (nval == 0) return;
+
+// fill-in the dummy index array if nvalp > 0;
+// otherwise move around the iorder sent down;
+// # (useful for ordering more than one vector);
+   for(int i = 1; i <= nval; i++){
+     
+       if(nvalp > 0) {
+         
+          iscrat.at(i - 1) = i;
+         
+       } else {
+         
+          iscrat.at(i - 1) = iorder.at(i - 1);
+       
+       }
+
+   }
+
+   if(debug::kprint > 7){
+     
+     Rcpp::Rcout << "\nsortii initial iscrat check\n" << std::endl;
+     Rcpp::Rcout << "iscrat = " << iscrat << std::endl;
+     
+   }
+
+// create ordered couples;
+   for(int iind = 2; iind <= nval; iind = iind + 2){
+     
+       if (icompi(iscrat.at(iind - 2), iscrat.at(iind - 1), xkey) <= 0) {
+         
+           iorder.at(iind - 2) = iscrat.at(iind - 2);
+           iorder.at(iind - 1) = iscrat.at(iind - 1);
+       
+         } else {
+         
+           iorder.at(iind - 2) = iscrat.at(iind - 1);
+           iorder.at(iind - 1) = iscrat.at(iind - 2);
+       
+       }
+
+   }
+
+if ((nval % 2) != 0) iorder.at(nval - 1) = iscrat.at(nval - 1);
+
+// we will now have ordered subsets a - b - a - b - ...;
+// and merge a and b couples into c - c - ...;
+   lmtnc = 2;
+   lmtna = 2;
+
+// iteration. each time, the length of the ordered subsets is doubled.;
+
+while(true){
+  
+  if (lmtna >= nval) break;
+  iwrkf = 0;
+  lmtnc = 2 * lmtnc;
+  iwrk = 0;
+
+// loop on merges of a and b into c;
+   while(true){
+     
+     iinda = iwrkf;
+     iwrkd = iwrkf + 1;
+     iwrkf = iinda + lmtnc;
+     jinda = iinda + lmtna;
+     if(iwrkf >= nval) {
+       
+        if(jinda >= nval) break;
+        iwrkf = nval;
+        
+     }
+     iindb = jinda;
+
+// shortcut for the case when the max of a is smaller;
+// than the min of b (no need to do anything);
+   if(icompi(iorder.at(jinda - 1),iorder.at(jinda),xkey) <= 0) {
+  
+      iwrk = iwrkf;
+      continue;
+      
+}
+
+// one steps in the c subset, that we create in the final rank array;
+   while(true){
+  
+   if(iwrk >= iwrkf) {
+   
+   // make a copy of the rank array for next iteration;
+   // iorder (iwrkd:iwrkf) = iscrat (iwrkd:iwrkf);
+      for(int i = iwrkd; i <= iwrkf; i++){
+        
+          iorder.at(i - 1) = iscrat.at(i - 1);
+        
+      }
+
+      break;
+   
+   }
+
+iwrk = iwrk + 1;
+
+// we still have unprocessed values in both a and b;
+if(iinda < jinda) {
+
+   if(iindb < iwrkf) {
+
+      if(icompi(iorder.at(iinda),iorder.at(iindb),xkey) > 0) {
+        
+         iindb = iindb + 1;
+         iscrat.at(iwrk - 1) = iorder.at(iindb - 1);
+         
+      } else {
+        
+         iinda = iinda + 1;
+         iscrat.at(iwrk - 1) = iorder.at(iinda - 1);
+      
+      }
+    
+   } else {
+
+      // only a still has unprocessed values;
+         iinda = iinda + 1;
+         iscrat.at(iwrk - 1) = iorder.at(iinda - 1);
+      
+   }
+
+} else {
+
+  // only b still has unprocessed values;
+  // iorder (iwrkd:iindb) = iscrat (iwrkd:iindb);
+     for(int i = iwrkd; i <= iindb; i++){
+        
+          iorder.at(i - 1) = iscrat.at(i - 1);
+        
+      }
+  
+     iwrk = iwrkf;
+     break;
+
+}
+
+}
+
+}
+
+// the cs become as and bs;
+   lmtna = 2 * lmtna;
+
+}
+
+return;
 
 }
