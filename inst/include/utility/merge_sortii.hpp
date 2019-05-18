2808f9bbb4f833d05e177096ea64afac9cbfcfcf
Auburngrads <auburngrads@live.com>
2019-05-18 13:19:15 -0400

    lots of fixes

---
+++ inst/include/utility/merge_sortii.hpp
@@ -0,0 +1,7 @@
+#ifndef SMRD_merge_sortii_H
+#define SMRD_merge_sortii_H
+void merge_sortii(Rcpp::IntegerVector &xkey, 
+                  int nvalp, 
+                  Rcpp::IntegerVector &iorder, 
+                  Rcpp::IntegerVector &iscrat);
+#endif
