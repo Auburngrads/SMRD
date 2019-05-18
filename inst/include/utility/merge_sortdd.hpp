9bda204a58b517f64d92a2e7582e89e554a0ef52
Auburngrads <auburngrads@live.com>
2019-05-18 13:19:12 -0400

    lots of fixes

---
+++ inst/include/utility/merge_sortdd.hpp
@@ -0,0 +1,7 @@
+#ifndef SMRD_merge_sortdd_H
+#define SMRD_merge_sortdd_H
+void merge_sortdd(Rcpp::NumericVector &xkey, 
+                  int nvalp, 
+                  Rcpp::IntegerVector &iorder, 
+                  Rcpp::IntegerVector &iscrat);
+#endif
