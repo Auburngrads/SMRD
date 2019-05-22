e112afd7b3af5b9cc160918fc2dbc0c992983297
Auburngrads <auburngrads@live.com>
2019-05-22 13:04:39 -0400

    fixed bug in SXCDF still working issue in SXPDF3

--- src/SXCDF_xcdf_fint1_rflcdf.cpp
+++ src/SXCDF_xcdf_fint1_rflcdf.cpp
@@ -6,7 +6,7 @@ double rflcdf(double x,
 double rfl_cdf = 0.0e00;
   
 // SEV distribution
-   if(ndist == 1) rfl_cdf = 1.e00 - std::exp(-1 * std::exp(x));
+   if(ndist == 1) rfl_cdf = 1.0e00 - (1 / std::exp(std::exp(x)));
 
 // Normal distribution
    if(ndist == 2) rfl_cdf = R::pnorm(x,0,1,true,false);
