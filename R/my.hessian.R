1d4ccde3d91d7bef1dac764229c973ec25e4f7b5
Auburngrads <auburngrads@live.com>
2019-05-22 13:02:29 -0400

    fixed bug in SXCDF still working issue in SXPDF3

--- R/my.hessian.R
+++ R/my.hessian.R
@@ -5,15 +5,21 @@ function (f.fun, theta, delta = 0.001 * theta)
     hessian <- matrix(NA, nrow = length(theta), ncol = length(theta))
     xnames <- rep(NA, 4)
     fun.value <- f.fun(theta)
+    
     for (i in 1:length(theta)) {
+        
         deltai <- delta[i] * identity[, i]
+        
         for (j in 1:i) {
+            
             deltaj <- delta[j] * identity[, j]
-            hessian[i, j] <- (f.fun(theta + deltai + deltaj) -
-                f.fun(theta + deltaj - deltai) - f.fun(theta -
-                deltaj + deltai) + f.fun(theta - deltaj - deltai))/(4 *
-                delta[i] * delta[j])
+            hessian[i, j] <- (f.fun(theta + deltai + deltaj) - 
+                              f.fun(theta + deltaj - deltai) - 
+                              f.fun(theta - deltaj + deltai) + 
+                              f.fun(theta - deltaj - deltai)) / 
+                             (4 * delta[i] * delta[j])
             hessian[j, i] <- hessian[i, j]
+            
             if (map.SMRDDebugLevel() >= 6) {
                 cat("\nIn my.hessian\n")
                 cat("i=", i, "j=", j, "theta=", theta[i], theta[j],
@@ -28,13 +34,16 @@ function (f.fun, theta, delta = 0.001 * theta)
                 xnames[3] <- "thetamp"
                 thetamm <- theta - deltaj - deltai
                 xnames[4] <- "thetamm"
-                fvec <- c(f.fun(thetapp), f.fun(thetapm), f.fun(thetamp),
-                  f.fun(thetamm))
+                fvec <- c(f.fun(thetapp), 
+                          f.fun(thetapm), 
+                          f.fun(thetamp),
+                          f.fun(thetamm))
                 names(fvec) <- xnames
                 print(fvec, digits = 9)
                 assign(envir = .frame0,  inherits = TRUE,"xdebug", F)
-            }
-            else {
+                
+            } else {
+                
                 assign(envir = .frame0,  inherits = TRUE,"xdebug", F)
             }
         }
