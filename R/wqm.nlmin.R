f970c3ac1a9086e1c916ae4a547e31f2e2a2c97b
Auburngrads <auburngrads@live.com>
2019-05-22 13:03:06 -0400

    fixed bug in SXCDF still working issue in SXPDF3

--- R/wqm.nlmin.R
+++ R/wqm.nlmin.R
@@ -1,12 +1,21 @@
 wqm.nlmin <-
-function (log.like, theta.start, dump, max.fcal = 900, max.iter = 150,
-    rfc.tol = 1e-10, xc.tol = 1e-10)
+function (log.like, 
+          theta.start, 
+          dump, 
+          max.fcal = 900, 
+          max.iter = 150,
+          rfc.tol = 1e-10, 
+          xc.tol = 1e-10)
 {
-    est.out <- optim(par = theta.start, fn = log.like, method = "BFGS",
-            control = list(maxit = max.iter, reltol = rfc.tol))
-        if (est.out$convergence > 0)
-            warning("Optim convergence problem code=", est.out$convergence)
-        results <- list(x = est.out$par, converge = est.out$convergence)
+    est.out <- optim(par = theta.start, 
+                     fn = log.like, 
+                     method = "BFGS",
+                     control = list(maxit = max.iter, reltol = rfc.tol))
+    
+        if (est.out$convergence > 0) warning("Optim convergence problem code = ", est.out$convergence)
+    
+        results <- list(x = est.out$par, 
+                        converge = est.out$convergence)
 
     return(results)
 }
