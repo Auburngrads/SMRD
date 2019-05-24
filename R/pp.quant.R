3c23c163de42023b1499f9ea2a9c564e0e2ab474
Auburngrads <auburngrads@live.com>
2019-05-24 10:03:01 -0400

    clean-up prior to testing powell algorithms

--- R/pp.quant.R
+++ R/pp.quant.R
@@ -4,8 +4,8 @@ function (p, distribution, shape = NULL)
     the.attributes <- attributes(p)
     
     answer <- `if`((is.null(shape) || distribution == "uniform"),
-                   SMRD2:::quant(p, distribution),
-                   SMRD2:::gquant(p, distribution, shape))
+                   quant(p, distribution),
+                   gquant(p, distribution, shape))
     
     attributes(answer) <- the.attributes
     return(answer)
