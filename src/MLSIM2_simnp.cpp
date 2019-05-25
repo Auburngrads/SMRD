734df7209bc402660986693ebf2905be35c56b16
Auburngrads <auburngrads@live.com>
2019-05-25 00:47:30 -0400

    lots of C++ fixes

--- src/MLSIM2_simnp.cpp
+++ src/MLSIM2_simnp.cpp
@@ -1,4 +1,14 @@
 #include <base/base.hpp>
+#include <boost/random.hpp>
+
+using boost::mt19937;
+using boost::uniform_01;
+
+double random01(mt19937 generator)
+{
+    static uniform_01<mt19937> dist(generator);
+    return dist();
+}
 
 // #nonparametric (resampling) bootstrap;
 // #simulate the data by sampling with replacement from the data;
@@ -11,8 +21,8 @@ void simnp(Rcpp::IntegerVector &iarray,
            Rcpp::IntegerVector &wtnew,
            int &nrow){
   
-double xran, mark;
-int jndex, index, itemp = 0;
+double xran;
+int jndex, index, itemp = 0,mark;
 wtnew = Rcpp::IntegerVector(nrow);
   
 if(method != 2) {
@@ -21,8 +31,10 @@ if(method != 2) {
    // and we will search thru each time
       for(int i = 1; i <= kount; i++){
         
-          xran = R::unif_rand();
-          index = xran * kount + 1;
+          boost::random::mt19937 rng; 
+          xran = random01(rng);
+          //xran = R::unif_rand();
+          index = std::floor(xran * kount) + 1;
           jndex = iarray.at(index - 1);
           wtnew.at(jndex - 1) = wtnew.at(jndex - 1) + 1;
         
@@ -48,10 +60,12 @@ if(method != 2) {
 // 25apr2009 Do we ever use the following?;
 for(int i = 1; i <= kount; i++){
   
-    xran = R::unif_rand();
-  
+    //xran = R::unif_rand();
+    boost::random::mt19937 rng; 
+    xran = random01(rng);
+    
     // mark will be between 1 and kount;
-       mark = xran * kount + 1;
+       mark = std::floor(xran * kount) + 1;
        
     for(jndex = 1; jndex <= nrow; jndex++){
       
@@ -59,16 +73,18 @@ for(int i = 1; i <= kount; i++){
       
         if(itemp >= mark) goto line45;
           
-        }
+    }
 
         jndex = 1;
              
+line45: wtnew.at(jndex - 1) = wtnew.at(jndex - 1) + 1;
+
         if(debug::kprint > 0) {
              
            Rcpp::Rcout << "\nSIMNP method 2\n" << std::endl;
            Rcpp::Rcout << "i = " << i << std::endl;
            Rcpp::Rcout << "kount = " << kount << std::endl;
-           Rcpp::Rcout << "jndex = " << jndex << std::endl;
+           Rcpp::Rcout << "jndex = " << jndex - 1 << std::endl;
            Rcpp::Rcout << "mark = " << mark << std::endl;
            Rcpp::Rcout << "itemp = " << itemp << std::endl;
            Rcpp::Rcout << "iarray(jndex) = " << iarray.at(jndex - 1) << std::endl;
@@ -77,8 +93,6 @@ for(int i = 1; i <= kount; i++){
              
         }
 
-    
-line45: wtnew.at(jndex - 1) = wtnew.at(jndex - 1) + 1;
 
 }
 
