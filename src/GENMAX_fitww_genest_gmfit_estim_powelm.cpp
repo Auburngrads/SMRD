ba45bcbf0abb72902502c26cf9621ddb3bc54a95
Auburngrads <auburngrads@live.com>
2019-05-25 00:47:21 -0400

    lots of C++ fixes

--- src/GENMAX_fitww_genest_gmfit_estim_powelm.cpp
+++ src/GENMAX_fitww_genest_gmfit_estim_powelm.cpp
@@ -26,8 +26,8 @@ void powelm(Rcpp::NumericVector &x,
             int &ier){
 
 double pz3 = 0.03e00,pz5 = 0.05e00,p1 = 0.1e00;
-double big = 1.0e30, epsilon = 1.0e-40;
-double emin,dscale,aaa = 0.0e00,scer,a,b;
+double big = 1.0e30,aaa = 0.0e00;
+double emin,dscale,scer,a,b;
 double sum,fkeep,fp,fa,fprev,fmval;
 int maxtry = 5,iprxxx,ipline;
 int j,jj,jjj,k,nfcc,inn,ind,iterc,isgrad,itone;
@@ -171,11 +171,11 @@ if(is == 4) goto line13;
 if(is == 5) goto line14;
 if(is == 6) goto line96;
 
-line14: if(std::abs(f - fa) < epsilon) goto line16;
+line14: if(std::abs(f - fa) == 0) goto line16;
         if(f < fa)       goto line15;
         if(f > fa)       goto line24;
         
-line16: if(std::abs(std::abs(d) - dmax) < epsilon) goto line17;
+line16: if(std::abs(std::abs(d) - dmax) == 0) goto line17;
         if(std::abs(d) > dmax) { ier = 1; goto line18; }
         
 line17: d = d + d;
@@ -208,12 +208,12 @@ line23: d = db + db - da;
 line83: d = 0.5 * (da + db - (fa - fb) / (da - db));
         is = 4;
 
-        if(std::abs((da - d) * (d - db)) < epsilon) goto line8;
+        if(std::abs((da - d) * (d - db)) == 0) goto line8;
         if(((da - d) * (d - db)) > 0.0) goto line8;
         
 line25: is = 1;
 
-        if(std::abs(std::abs(d - db) - ddmax) < epsilon) goto line8;
+        if(std::abs(std::abs(d - db) - ddmax) == 0) goto line8;
         if(std::abs(d - db) < ddmax) goto line8;
         
 line26: d = db + dsign(ddmax,(db - da));
@@ -233,13 +233,13 @@ line26: d = db + dsign(ddmax,(db - da));
         dscale = dscale * 4.0; //factor
         goto line1001;
         
-line265: if(std::abs(ddmax - dmax) < epsilon) goto line8;
+line265: if(std::abs(ddmax - dmax) == 0) goto line8;
          if(ddmax < dmax) goto line8;
 
          ddmax = dmax;
          goto line8;
 
-line13: if(std::abs(f - fa) < epsilon) goto line23;
+line13: if(std::abs(f - fa) == 0) goto line23;
         if(f > fa) goto line23;
 
 line28: fc = fb;
@@ -249,14 +249,14 @@ line29: fb = f;
         db = d;
         goto line30;
 
-line12: if(std::abs(f - fb) < epsilon) goto line28;
+line12: if(std::abs(f - fb) == 0) goto line28;
         if(f < fb) goto line28;
 
         fa = f;
         da = d;
         goto line30;
 
-line11: if(std::abs(f - fb) < epsilon) goto line10;
+line11: if(std::abs(f - fb) == 0) goto line10;
         if(f > fb) goto line10;
 
         fa = fb;
@@ -277,7 +277,7 @@ line10: fc = f;
 line30: a = (db - dc) * (fa - fc);
         b = (dc - da) * (fb - fc);
 
-        if(std::abs((a + b) * (da - dc)) < epsilon) goto line33;
+        if(std::abs((a + b) * (da - dc)) == 0) goto line33;
         if(((a + b) * (da - dc)) > 0.0) goto line34;
 
 line33: fa = fb;
@@ -290,7 +290,7 @@ line34: d = 0.5 * (a * (db + dc) + b * (da + dc)) / (a + b);
         di = db;
         fi = fb;
 
-        if(std::abs(fb - fc) < epsilon) goto line44;
+        if(std::abs(fb - fc) == 0) goto line44;
         if(fb < fc) goto line44;
         di = dc;
         fi = fc;
@@ -302,13 +302,13 @@ line44: if(itone == 1) goto line86;
 line85: itone = 2;
         goto line45;
 
-line86: if(std::abs(std::abs(d - di) - dacc) < epsilon) goto line41;
+line86: if(std::abs(std::abs(d - di) - dacc) == 0) goto line41;
         if(std::abs(d - di) < dacc) goto line41;
 
-        if(std::abs(std::abs(d - di) - (pz3 * std::abs(d))) < epsilon) goto line41;
+        if(std::abs(std::abs(d - di) - (pz3 * std::abs(d))) == 0) goto line41;
         if(std::abs(d - di) < (pz3 * std::abs(d))) goto line41;
         
-line45: if(std::abs((da - dc) * (dc - d)) < epsilon) goto line46;
+line45: if(std::abs((da - dc) * (dc - d)) == 0) goto line46;
         if(((da - dc) * (dc - d)) < 0.0) goto line47;
 
 line46: fa = fb;
@@ -318,7 +318,7 @@ line46: fa = fb;
         goto line25;
 
 line47: is = 2;
-        if(std::abs((db - d) * (d - dc)) < epsilon) goto line8;
+        if(std::abs((db - d) * (d - dc)) == 0) goto line8;
         if(((db - d) * (d - dc)) > 0.0) goto line8;
 
         is = 3;
@@ -375,7 +375,7 @@ line557: if(iprxxx == 1) goto line51;
 line51:  if(itone == 1) goto line55;
          if(itone == 2) goto line38;
 
-line55: if(std::abs((fprev - f) - sum) < epsilon) goto line95;
+line55: if(std::abs((fprev - f) - sum) == 0) goto line95;
         if((fprev - f) < sum) goto line94;
 
 line95: sum = fprev - f;
@@ -403,12 +403,12 @@ line92: fhold = f;
 line96: if(ind == 1) goto line112;
         if(ind == 2) goto line87;
 
-line112: if(std::abs(fp - f) < epsilon) goto line37;
+line112: if(std::abs(fp - f) == 0) goto line37;
          if(fp < f) goto line37;
 
          d = 2 * (fp + f - 2 * fhold) / std::pow((fp - f),2);
 
-         if(std::abs((d * std::pow((fp - fhold - sum),2)) - sum) < epsilon) goto line37;
+         if(std::abs((d * std::pow((fp - fhold - sum),2)) - sum) == 0) goto line37;
          if(((d * std::pow((fp - fhold - sum),2)) - sum) > 0.0) goto line37;
 
 line87: j = jtl * n + 1;
@@ -438,7 +438,7 @@ for(int i = 1; i <= n; i++){
     krxp = krxp + 1;
     w.at(k - 1) = w.at(krxp - 1);
 
-    if(std::abs(aaa - std::abs(w.at(k - 1) / e.at(i - 1))) < epsilon) goto line67;
+    if(std::abs(aaa - std::abs(w.at(k - 1) / e.at(i - 1))) == 0) goto line67;
     if(aaa > std::abs(w.at(k - 1) / e.at(i - 1))) goto line67;
 
     aaa = std::abs(w.at(k - 1) / e.at(i - 1));
@@ -474,7 +474,7 @@ for(int i = 1; i <= n; i++){
 
     krxp = krxp + 1;
     x.at(i - 1) = x.at(i - 1) - w.at(krxp - 1);
-    if(std::abs((aaa * std::abs(e.at(i - 1))) - std::abs(w.at(krxp - 1))) < epsilon) continue;
+    if(std::abs((aaa * std::abs(e.at(i - 1))) - std::abs(w.at(krxp - 1))) == 0) continue;
     if((aaa * std::abs(e.at(i - 1))) > std::abs(w.at(krxp - 1))) continue;
 
     aaa = std::abs(w.at(krxp - 1) / e.at(i - 1));
@@ -493,7 +493,7 @@ line72: if(iprxxx >= 2) goto line50;
 line53: if(ind == 1) goto line109;
         if(ind == 2) goto line88;
         
-line109: if(std::abs(aaa - p1) < epsilon) goto line89;
+line109: if(std::abs(aaa - p1) == 0) goto line89;
          if(aaa > p1) goto line76;
 
 line89: if(icon == 1) goto line20;
@@ -521,7 +521,7 @@ nfcc = nfcc + 1;
 ddmag = 0.0;
 goto line108;
 
-line76: if(std::abs(f - fp) < epsilon) goto line78;
+line76: if(std::abs(f - fp) == 0) goto line78;
         if(f < fp) goto line35;
         
 line78: if((emin > 1.0e-05) or (debug::kprint >= 3)) {
@@ -542,7 +542,7 @@ line108: iterc = iterc + 1;
          if(iterc <= maxit) goto line5;
          
         Rcpp::warning("powelm: %i iterations completed without meeting specifications", maxit);
-        if(std::abs(f - fkeep) < epsilon) goto line20;
+        if(std::abs(f - fkeep) == 0) goto line20;
         if(f < fkeep) goto line20;
 
         f = fkeep;
@@ -559,7 +559,7 @@ line108: iterc = iterc + 1;
 line101: jtl = 1;
          fp = fkeep;
 
-         if(std::abs(f - fkeep) < epsilon) goto line78;
+         if(std::abs(f - fkeep) == 0) goto line78;
          if(f < fkeep)          goto line105;
          if(f > fkeep)          goto line104;
 
@@ -585,7 +585,7 @@ for(int i = 1; i <= n; i++){
 jtl = 2;
 goto line92;
 
-line106: if(std::abs(aaa - p1) < epsilon) goto line20;
+line106: if(std::abs(aaa - p1) == 0) goto line20;
          if(aaa > p1) goto line107;
 
 line20:  return;
