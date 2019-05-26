9db09991ea02d85a2727956564aa371d556198aa
Auburngrads <auburngrads@live.com>
2019-05-26 18:54:11 -0400

    Updated `Response` to correctly select the `time.column` as the responses for unfolded data types

--- R/Response.R
+++ R/Response.R
@@ -26,8 +26,12 @@ switch(frame.type[1],
         
     }, unfolded = {
       
-        the.response <- data.d$y
-        if (is.null(the.response)) the.response <- data.d$Response
+          response.column <- attr(data.d, "time.column")
+          if (is.character(data.d) && length(data.d == 1)) data.d <- get(envir = .frame0, inherits = TRUE, data.d)
+          the.response <- as.matrix(data.d[, response.column])
+          col.names <- dimnames(data.d)[[2]]
+          names(col.names) <- col.names
+          dimnames(the.response) <- list(NULL, col.names[response.column])
         
     }, {
         stop("Corrupted data frame")
@@ -52,6 +56,7 @@ function (data.ld, value)
   } else {
     
     if (ncol(value) == 2 && ncol(the.oldResponse) == 1) {
+      
        Response.names <- paste(c(dimnames(the.oldResponse)[[2]],
                                  dimnames(the.oldResponse)[[2]]), 
                                  c("L", "U"),
@@ -60,7 +65,7 @@ function (data.ld, value)
           
        `if`(ncol(value) == 1 && ncol(the.oldResponse) ==  1,
             Response.names <- dimnames(the.oldResponse)[[2]][1],
-            stop(paste("BadResponse new col =", 
+            stop(paste("Bad Response new col =", 
                        ncol(value),
                        "and old col =", 
                         ncol(the.oldResponse))))
@@ -73,7 +78,7 @@ function (data.ld, value)
      Response.length <- nrow(value)
      
       if (Response.length != nrow(data.ld))
-          stop(paste("InsertingResponse  with length",
+          stop(paste("Inserting Response with length",
                      Response.length,
                      "but data objects had", 
                      nrow(data.ld), 
@@ -82,11 +87,11 @@ function (data.ld, value)
       the.response.columns <- attr(data.ld, "response.column")
       all.attributes <- attributes(data.ld)
       frame.names <- names(data.ld)
-      response.col.numbers <- match(names(data.ld[, the.response.columns,
-                                                  drop = F]), frame.names)
+      response.col.numbers <- match(names(data.ld[, the.response.columns, drop = F]), 
+                                    frame.names)
+      
       `if`(ncol(data.ld) > length(response.col.numbers),
-           new.frame <- data.frame(data.ld[, -response.col.numbers, drop = F], 
-                                   value),
+           new.frame <- data.frame(data.ld[, -response.col.numbers, drop = F], value),
            new.frame <- data.frame(value))
       
       all.attributes$response.column <- dimnames(value)[[2]]
