00e8d4f33054757c0daaf04e57fbd2f2a581d02c
Auburngrads <auburngrads@live.com>
2019-05-26 01:09:59 -0400

    completed SXPDF3 and added lots of other updates

--- R/get.alt.plan.values.from.slope.and.point.R
+++ R/get.alt.plan.values.from.slope.and.point.R
@@ -1,3 +1,61 @@
+#' Title
+#'
+#' @param distribution 
+#' @param relationship 
+#' @param slope 
+#' @param probs 
+#' @param accelvar 
+#' @param censor.time 
+#' @param beta 
+#' @param sigma 
+#' @param time.units 
+#' @param accelvar.units 
+#' @param power 
+#' @param use.conditions 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' AdhesiveBond.Weibull.altpv <- get.alt.plan.values.from.slope.and.point(distribution = "Weibull",
+#'                                                                        relationship = "Arrhenius", 
+#'                                                                        accelvar.units = c("DegreesC"),
+#'                                                                        time.units = "Days", 
+#'                                                                        censor.time = 183,
+#'                                                                        probs = c(.001), 
+#'                                                                        accelvar = c(50),
+#'                                                                        slope = 0.726, 
+#'                                                                        beta = 1.667)
+#' 
+#' print(AdhesiveBond.Weibull.altpv)
+#' 
+#' AdhesiveBond1.altplan <- get.alt.test.plan.direct(accel.variable.levels = c(78,98,120),
+#'                                                   number.of.units = c(155,60,84),
+#'                                                   censor.times = c(183,183,183))
+#' 
+#' AdhesiveBond2.altplan <- get.alt.test.plan.direct(accel.variable.levels = c(80,100,120),
+#'                                                   number.of.units = c(212,88,0),
+#'                                                   censor.times = c(243,243,243))
+#' 
+#' AdhesiveBond3.altplan <- get.alt.test.plan.direct(accel.variable.levels = c(80,100,120),
+#'                                                   number.of.units = c(150,60,90),
+#'                                                   censor.times = c(243,243,243))
+#' 
+#' plot(AdhesiveBond1.altplan,
+#'      ALT.plan.values = AdhesiveBond.Weibull.altpv,
+#'      use.condition = 50)
+#' 
+#' plot(AdhesiveBond2.altplan,
+#'      ALT.plan.values = AdhesiveBond.Weibull.altpv,
+#'      use.condition = 50)
+#' 
+#' plot(AdhesiveBond3.altplan,
+#'      ALT.plan.values = AdhesiveBond.Weibull.altpv,
+#'      use.condition = 50)
+#' 
+#' }
 get.alt.plan.values.from.slope.and.point <-
 function (distribution, relationship, slope, probs, accelvar, 
     censor.time, beta, sigma, time.units = "Time", accelvar.units, 
