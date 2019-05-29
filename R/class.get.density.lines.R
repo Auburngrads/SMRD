08998e19f9682538a02549ab39a03da5ec0e2fd8
Auburngrads <auburngrads@live.com>
2019-05-29 17:08:00 -0400

    fixed functions prefixed with '.' - why did I do that? added exports and documents through echapter24

--- R/class.get.density.lines.R
+++ R/class.get.density.lines.R
@@ -7,7 +7,7 @@ function (class.level, density.at, theta.hat, sigma, distribution,
     q.low <- quant(0.01, distribution)
     q.high <- quant(0.99, distribution)
     zvec <- seq(q.low, q.high, length = 50)
-    xden <- (.Uminus(scale.factor) * wqmf.phis(zvec, distribution))/sigma
+    xden <- (Uminus(scale.factor) * wqmf.phis(zvec, distribution))/sigma
     yden <- (mu + zvec * sigma)
     return(list(xden = xden, yden = yden, at = density.at))
 }
