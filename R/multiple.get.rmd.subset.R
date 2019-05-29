0f4b8bbe7a04b1f9893b36a8f47a2cd831660e66
Auburngrads <auburngrads@live.com>
2019-05-29 17:09:40 -0400

    fixed functions prefixed with '.' - why did I do that? added exports and documents through echapter24

--- R/multiple.get.rmd.subset.R
+++ R/multiple.get.rmd.subset.R
@@ -4,6 +4,6 @@ function (data.rmd, markers, columns = 1:length(attr(data.rmd,
 {
     if (is.null(attr(data.rmd, "x.columns")))
         stop(paste("No xmat in:", get.data.title(data.rmd)))
-    right.stuff <- get.the.right.stuff(data.rmd, columns, markers)
+    right.stuff <- get.theright.stuff(data.rmd, columns, markers)
     invisible(get.rmd.subset(data.rmd, right.stuff))
 }
