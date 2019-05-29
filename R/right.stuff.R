3608e5bfe239af89a566f49ce5779f1c8bc9a99c
Auburngrads <auburngrads@live.com>
2019-05-29 17:10:17 -0400

    fixed functions prefixed with '.' - why did I do that? added exports and documents through echapter24

--- R/right.stuff.R
+++ R/right.stuff.R
@@ -1,4 +1,4 @@
-.right.stuff <-
+right.stuff <-
 function (data.ld)
 {
     return(attr(data.ld, "right.stuff"))
@@ -7,7 +7,7 @@ function (data.ld)
 #
 #
 
-`.right.stuff<-` <-
+`right.stuff<-` <-
   function (data.ld, value)
   {
     attr(data.ld, "right.stuff") <- value
