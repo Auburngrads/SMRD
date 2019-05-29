22c31c8f4f08e8f5ea374ec78ff0c67a7afec3eb
Auburngrads <auburngrads@live.com>
2019-05-29 17:09:44 -0400

    fixed functions prefixed with '.' - why did I do that? added exports and documents through echapter24

--- R/my.write.R
+++ R/my.write.R
@@ -1,4 +1,4 @@
-.my.write <-
+my.write <-
 function (x, file = "data", ncolumns = if (is.character(x)) 1 else 5,
     append = FALSE, sep = " ")
 cat(x, sep = c(rep(sep, ncolumns - 1), "\n"), file = file, append = append)
