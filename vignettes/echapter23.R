b8fa7eb0c6e6639622cca1b93fe8b5b048061551
Auburngrads <auburngrads@live.com>
2019-05-29 08:47:26 -0400

    cleaning up and fixing vignettes

--- vignettes/echapter23.R
+++ vignettes/echapter23.R
@@ -2,3 +2,16 @@
 SMRD2:::vinny()
 library(SMRD2)
 
+## ------------------------------------------------------------------------
+## create the ddd data object
+
+Insulation.ddd <- frame.to.ddd(insulation,
+                               response.column = 3, 
+                               time.column = 1,
+                               x.columns = 2,
+                               data.title = "Voltage Breakdown Data",
+                               response.units = "Volts",
+                               time.units = "Weeks")
+
+print(Insulation.ddd)
+
