1d612013ad87389540564371741fb2693d7e5a31
Auburngrads <auburngrads@live.com>
2019-05-29 15:20:32 -0400

    lots of updates revolving around echapter24

--- R/plot.ADDT.test.plan.R
+++ R/plot.ADDT.test.plan.R
@@ -18,16 +18,27 @@ function (x,...)
     the.allocations <- allocation(x)
     the.times <- times(x)
     the.levels <- AT.levels(x)
+    
     if (ncol(the.levels) > 1) {
         cat("No plot for multiple variable plans\n")
         invisible(NULL)
     }
-    plot.paper(xlim = range(the.times), ylim = range(the.levels[[1]]),
-        x.axis = "linear", y.axis = "linear", ylab = names(the.levels),
-        xlab = dimnames(the.times)[2][1], cex = 1.5, cex.labs = 1.5,
-        grids = F)
+    
+    plot.paper(xlim = range(the.times), 
+               ylim = range(the.levels[[1]]),
+               x.axis = "linear", 
+               y.axis = "linear", 
+               ylab = names(the.levels),
+               xlab = dimnames(the.times)[2][1], 
+               cex = 1.5, 
+               cex.labs = 1.5,
+               grids = F)
+    
     ntimes <- ncol(the.times)
-    text((the.times[, 1] + the.times[, ntimes])/2, the.levels[[1]],
-        the.allocations[[1]])
+    
+    text((the.times[, 1] + the.times[, ntimes]) / 2, 
+         the.levels[[1]],
+         the.allocations[[1]])
+    
     invisible()
 }
