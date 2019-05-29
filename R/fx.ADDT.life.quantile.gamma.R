67c8e3336ac6874f0ea0868c5f7aaa276adadecf
Auburngrads <auburngrads@live.com>
2019-05-29 17:08:51 -0400

    fixed functions prefixed with '.' - why did I do that? added exports and documents through echapter24

--- R/fx.ADDT.life.quantile.gamma.R
+++ R/fx.ADDT.life.quantile.gamma.R
@@ -2,7 +2,7 @@ fx.ADDT.life.quantile.gamma <-
 function (gamma.hat, p, distribution, FailLevel, xuse, transformation.response, 
     transformation.x, transformation.time, model, trans.of.quantile = F) 
 {
-    theta.hat <- .f.ADDT.origparam(gamma.hat, model)
+    theta.hat <- f.ADDT.origparam(gamma.hat, model)
     return(fx.ADDT.life.quantile(theta.hat, p, distribution, 
         FailLevel, xuse, transformation.response, transformation.x, 
         transformation.time = transformation.time, trans.of.quantile = trans.of.quantile))
