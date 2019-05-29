65e6a1133379d88aa451ee60dba35fd75c09e80b
Auburngrads <auburngrads@live.com>
2019-05-29 08:46:56 -0400

    cleaning up and fixing vignettes

--- vignettes/echapter17.R
+++ vignettes/echapter17.R
@@ -14,40 +14,35 @@ summary(comptime.ld)
 censored.data.plot(comptime.ld,
                    xlab = "System Load")
 
-## ------------------------------------------------------------------------
-comptime.mlest.out <- groupm.mleprobplot(comptime.ld, 
-                                         distribution ="Lognormal", 
-                                         group.var = 1, 
-                                         relationship = "linear")
-
-comptime.mlest.out2 <- groupm.mleprobplot(comptime.ld, 
-                                          distribution ="Normal", 
-                                          group.var = 1, 
-                                          relationship = "linear")
-
-## ------------------------------------------------------------------------
-Snubber.ld <- frame.to.ld(snubber, 
-                          response.column = "cycles", 
-                          censor.column = "event",
-                          time.units = "Cycles",
-                          case.weight.column = "count",
-                          x.columns = "design")
-
-## ------------------------------------------------------------------------
-ZelenCap.ld <- frame.to.ld(zelencap,
-                           response.column = 1,
-                           censor.column = 2,
-                           case.weight.column = 3,
-                           x.columns = c(4, 5),
-                           time.units = "Hours")
-
-## ------------------------------------------------------------------------
-ZelenCap.groupi.Weibull.out <- 
-  groupi.mleprobplot(ZelenCap.ld, 
-                     distribution = "Weibull", 
-                     group.var = c(1, 2))
-
-summary(ZelenCap.groupi.Weibull.out)
-
-names(xmat(ZelenCap.ld))
+## ---- eval=FALSE---------------------------------------------------------
+#  comptime.mlest.out <- groupm.mleprobplot(comptime.ld,
+#                                           distribution ="Lognormal",
+#                                           group.var = 1,
+#                                           relationship = "linear")
+#  
+#  comptime.mlest.out2 <- groupm.mleprobplot(comptime.ld,
+#                                            distribution ="Normal",
+#                                            group.var = 1,
+#                                            relationship = "linear")
+#  
+#  SMRD2:::resid.vs.order(comptime.mlest.out)
+#  
+#  SMRD2:::resid.vs.fit(comptime.mlest.out)
+#  
+#  SMRD2:::resid.vs.explan(comptime.mlest.out)
+#  
+#  SMRD2:::resid.probplot(comptime.mlest.out)
+#  
+#  plot(comptime.mlest.out,
+#       density.at = c(1, 3, 5))
+#  
+#  plot(comptime.mlest.out,
+#       density.at = c(1, 3, 5),
+#       response.on.yaxis = F)
+#  
+#  #or more simply as
+#  
+#  plot(comptime.mlest.out)
+#  
+#  quantiles(comptime.mlest.out,new.data=5)
 
