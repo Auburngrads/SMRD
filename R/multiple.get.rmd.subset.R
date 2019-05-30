b32c4b76a75e17fe3eeea152fb6ffd6db0ae3c37
Auburngrads <auburngrads@live.com>
2019-05-30 17:26:46 -0400

    lots more stuff

--- R/multiple.get.rmd.subset.R
+++ R/multiple.get.rmd.subset.R
@@ -1,9 +1,16 @@
 multiple.get.rmd.subset <-
-function (data.rmd, markers, columns = 1:length(attr(data.rmd,
-    "x.columns")))
+function (data.rmd, 
+          markers,
+          columns = 1:length(attr(data.rmd,"x.columns")))
 {
-    if (is.null(attr(data.rmd, "x.columns")))
+    if (is.null(attr(data.rmd, "x.columns"))){
+        
         stop(paste("No xmat in:", get.data.title(data.rmd)))
-    right.stuff <- get.theright.stuff(data.rmd, columns, markers)
+        
+    }
+    
+    right.stuff <- get.the.right.stuff(data.rmd, columns, markers)
+    
     invisible(get.rmd.subset(data.rmd, right.stuff))
+    
 }
