2f1437fdbb381795f7d0f57844549a08d8d5e3cd
Auburngrads <auburngrads@live.com>
2019-05-30 17:25:35 -0400

    lots more stuff

--- R/Fr.conf.R
+++ R/Fr.conf.R
@@ -1,11 +1,86 @@
+#' Title
+#'
+#' @param fcn 
+#' @param fcn.arg2 
+#' @param gmle.out 
+#' @param extrapolate 
+#' @param ptwise.if.simult 
+#' @param conf.level 
+#' @param ... 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' Fan.ld <- frame.to.ld(fan,
+#'                       response.column = 1, 
+#'                       censor.column = 2, 
+#'                       case.weight.column = 3,
+#'                       time.units = "Hours")
+#' 
+#' Fan.egeng.gmle.out <- FillRegion(Fan.egeng.gmle.out,
+#'                                  nbound = 10,
+#'                                  iter = 500)
+#' 
+#' summary(Fan.egeng.gmle.out.jcr)
+#' 
+#' names(Fan.egeng.gmle.out.jcr)
+#' 
+#' basic.gmleprobplot(Fan.ld,distribution = "egeng",
+#'                    xlim = c(200,99999),
+#'                    ylim = c(.0011,.69),
+#'                    xxx.mle.out = Fan.egeng.gmle.out,
+#'                    my.title = "",
+#'                    cexlab = 1.5,
+#'                    conlev = .95,
+#'                    ciMethod = "lr.approx",
+#'                    length.time.vec = 2)
+#' 
+#' Fan.weibull.gmle.out <- ls2.mle(Fan.ld, distribution = "weibull")
+#' 
+#' 
+#' Fan.weibull.gmle.out <-  FillRegion(Fan.weibull.gmle.out,
+#'                                     nbound = 4,
+#'                                     iter = 50,
+#'                                     cull = 2 )
+#' 
+#' fcn = function(theta,time,distribution){
+#'   
+#'       f.phibf((log(time)-theta[1]) / exp(theta[2]),
+#'                       distribution = "Weibull")
+#'   
+#' }
+#' 
+#' Fr.conf(fcn, 
+#'         fcn.arg2 = log.seq(200,2000,length=10),
+#'         gmle.out = Fan.weibull.gmle.out,
+#'         ptwise = T)
+#' 
+#' Fr.conf(fcn,
+#'         fcn.arg2 = log.seq(200,2000,length = 10),
+#'         gmle.out = Fan.weibull.gmle.out,  
+#'         ptwise = T, 
+#'         extrapolate = T)
+#' 
+#' }
 Fr.conf <-
-function (fcn, fcn.arg2, gmle.out, extrapolate = F, ptwise.if.simult = F,
-    conf.level = GetSMRDDefault("SMRD.ConfLevel")/100, ...)
+function (fcn, 
+          fcn.arg2, 
+          gmle.out, 
+          extrapolate = F, 
+          ptwise.if.simult = F,
+          conf.level = GetSMRDDefault("SMRD.ConfLevel")/100, ...)
 {
     jcrname <- gmle.out$jcrname
-    if (is.null(gmle.out$jcrname) && !exists(jcrname))
-        stop("The joint likelihood ratio confidence region (jcrname) \n                       is not available")
-    jcr <- get(envir = .frame0, jcrname)
+    
+    if (is.null(gmle.out$jcrname) && !exists("jcrname")){
+      
+        stop("The joint likelihood ratio confidence region (jcrname) \nis not available")
+      
+    }
+    jcr <- eval(parse(text = jcrname))
     param <- jcr$conf.reg
     type <- jcr$type
     l.bounds <- jcr$l.bounds
