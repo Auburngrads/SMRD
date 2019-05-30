42ece53fec8def7953f8162889c6f50047d9f4a8
Auburngrads <auburngrads@live.com>
2019-05-30 17:26:14 -0400

    lots more stuff

--- R/get.data.title.R
+++ R/get.data.title.R
@@ -2,9 +2,11 @@ get.data.title <-
 function (data.d) 
 {
     data.title <- attr(data.d, "data.title")
-    if (is.null(data.title)) 
-        data.title <- data.d$data.title
-    if (is.null(data.title)) 
-        data.title <- data.d$title
+    
+    if (is.null(data.title)) data.title <- data.d$data.title
+    
+    if (is.null(data.title)) data.title <- data.d$title
+    
     return(data.title)
+    
 }
