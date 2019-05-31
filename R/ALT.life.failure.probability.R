26cc96fce0d2f62ff27fa75627df45c332fa1579
Auburngrads <auburngrads@live.com>
2019-05-31 17:00:41 -0400

    DONE WITH VIGNETTES!!!!

--- R/ALT.life.failure.probability.R
+++ R/ALT.life.failure.probability.R
@@ -1,12 +1,14 @@
 ALT.life.failure.probability <-
-function (theta.hat, time.vec, distribution, xuse.tran) 
+function (theta.hat, 
+          time.vec, 
+          distribution, 
+          xuse.tran) 
 {
     nter <- length(theta.hat) - 1
-    mu <- theta.hat[1] + as.matrix(xuse.tran) %*% as.matrix(theta.hat[2:nter], 
-        ncol = 1)
-    if (is.logdist(distribution)) 
-        time.vec <- logb(time.vec)
-    zvec <- (time.vec - mu)/theta.hat[length(theta.hat)]
+    mu <- theta.hat[1] + as.matrix(xuse.tran) %*% as.matrix(theta.hat[2:nter],
+                                                            ncol = 1)
+    if (is.logdist(distribution)) time.vec <- logb(time.vec)
+    zvec <- (time.vec - c(mu)) / theta.hat[length(theta.hat)]
     answers <- wqmf.phibf(zvec, distribution)
     return(answers)
 }
