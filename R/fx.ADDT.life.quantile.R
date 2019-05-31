0369159baceb3e85955d713da03f3f46f76a9b56
Auburngrads <auburngrads@live.com>
2019-05-31 17:01:11 -0400

    DONE WITH VIGNETTES!!!!

--- R/fx.ADDT.life.quantile.R
+++ R/fx.ADDT.life.quantile.R
@@ -1,21 +1,42 @@
 fx.ADDT.life.quantile <-
-function (theta.hat, p, distribution, FailLevel, xuse, transformation.response, 
-    transformation.x, transformation.time = transformation.time, 
-    trans.of.quantile = F, dump = F) 
+function (theta.hat, 
+          p, 
+          distribution, 
+          FailLevel, 
+          xuse, 
+          transformation.response, 
+          transformation.x, 
+          transformation.time = transformation.time, 
+          trans.of.quantile = F, 
+          dump = F) 
 {
     x.tran <- xuse
+    
     for (i in 1:ncol(xuse)) {
-        x.tran[, i] <- f.relationship(xuse[, i], subscript.relationship(transformation.x, 
-            i))
+        
+        x.tran[, i] <- f.relationship(xuse[, i], 
+                                      subscript.relationship(transformation.x, i))
+        
     }
-    answer <- ADDT.life.quantile(p, distribution, f.relationship(FailLevel, 
-        transformation.response), x.tran, theta.hat)
+    
+    answer <- ADDT.life.quantile(p, 
+                                 distribution, 
+                                 f.relationship(FailLevel, transformation.response), 
+                                 x.tran, 
+                                 theta.hat)
+    
     if (dump) {
-        cat("\n\ntheta.hat=", paste(theta.hat, collapse = ","), 
+        
+        cat("\n\ntheta.hat=", 
+            paste(theta.hat, collapse = ","), 
             "p=", p, "x=", xuse, "ans=", answer, "\ntr,tx=", 
-            transformation.response, transformation.x, "in fx.ADDT.life.quantile\n")
+            transformation.response, 
+            transformation.x, "in fx.ADDT.life.quantile\n")
+        
     }
-    if (!trans.of.quantile) 
-        answer <- f.relationshipinv(answer, transformation.time)
+    
+    if (!trans.of.quantile) answer <- f.relationshipinv(answer, transformation.time)
+    
     return(answer)
+    
 }
