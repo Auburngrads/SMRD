7e4cd3d80a3ace8ad0f412b871d2518c14ee3d10
Auburngrads <auburngrads@live.com>
2019-05-30 23:47:10 -0400

    only three vignettes left

--- R/CondProbInterval.R
+++ R/CondProbInterval.R
@@ -1,3 +1,50 @@
+#' Title
+#'
+#' @param mu 
+#' @param sigma 
+#' @param distribution 
+#' @param age 
+#' @param tL 
+#' @param tU 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' BearingCage.ld <- frame.to.ld(bearingcage,
+#'                               response.column = 1, 
+#'                               censor.column = 2,
+#'                               case.weight.column = 3)
+#' 
+#' BearingCage.mlest.weib <- mlest(BearingCage.ld,
+#'                                 dist = "Weibull")
+#' 
+#' CondProbInterval2(BearingCage.mlest.weib,
+#'                   age = 50,
+#'                   tL = 50,
+#'                   tU = 350)
+#' 
+#' CondProbInterval2(BearingCage.mlest.weib,
+#'                   age = 150,
+#'                   tL = 150,
+#'                   tU = 450)
+#' 
+#' # Table 12.1 from Meeker and Escobar (1998)
+#' 
+#' PredictTable(BearingCage.mlest.weib,
+#'              FtimeStart = 0, 
+#'              FtimeEnd = 300)
+#' 
+#' CondProbInterval(mu = 3.7393, 
+#'                  sigma = 0.7639,
+#'                  distribution = "lognormal",
+#'                  age = 1,
+#'                  tL = 1,
+#'                  tU = 36)
+#' 
+#' }
 CondProbInterval <-
 function (mu, sigma, distribution, age, tL, tU) 
 {
