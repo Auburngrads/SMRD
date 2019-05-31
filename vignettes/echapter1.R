ef685990b79afdf79dfa3613371f141d9331ee9b
Auburngrads <auburngrads@live.com>
2019-05-30 23:47:33 -0400

    only three vignettes left

--- vignettes/echapter1.R
+++ vignettes/echapter1.R
@@ -19,3 +19,30 @@ print(lzbearing.ld)
 
 plot(lzbearing.ld, distribution = "weibull")
 
+## ------------------------------------------------------------------------
+superalloy.ld <- frame.to.ld(superalloy,
+                             response.column = 1, 
+                             censor.column = 2,
+                             x.columns = c(5,6,4),
+                             time.units = "Kilocycles")
+
+summary(superalloy.ld)
+
+censored.data.plot(superalloy.ld, 
+                   explan.var = 1)
+
+censored.data.plot(superalloy.ld, 
+                   explan.var = 3,
+                   response.on.yaxis = F)
+
+censored.data.plot(superalloy.ld, 
+                   explan.var = 3, 
+                   x.axis = "log", 
+                   y.axis = "log")
+
+censored.data.plot(superalloy.ld, 
+                   explan.var = 3,
+                   response.on.yaxis = F, 
+                   x.axis = "log", 
+                   y.axis = "log")
+
