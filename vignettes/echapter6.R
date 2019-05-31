8c9e415106941f038652cb878745abb79665a2b3
Auburngrads <auburngrads@live.com>
2019-05-30 23:51:21 -0400

    only three vignettes left

--- vignettes/echapter6.R
+++ vignettes/echapter6.R
@@ -64,3 +64,92 @@ titanium.ld <- frame.to.ld(titanium2,
 plot(titanium.ld,
      distribution = "Lognormal")
 
+## ------------------------------------------------------------------------
+Bleed.ld <- frame.to.ld(bleed,
+                        response.column = 1, 
+                        censor.column = 2, 
+                        case.weight.column = 3,
+                        x.columns = 4,
+                        time.units = "Hours")
+
+Bleed.ld_D <- ld.split(Bleed.ld, stress.var = "D")
+Bleed.ld_Other <- ld.split(Bleed.ld, stress.var = "Other")
+
+## ---- fig.height=9-------------------------------------------------------
+event.plot(Bleed.ld)
+summary(Bleed.ld)
+
+## ---- fig.height=6-------------------------------------------------------
+event.plot(Bleed.ld_D)
+summary(Bleed.ld_D)
+
+event.plot(Bleed.ld_Other)
+summary(Bleed.ld_Other)
+
+## ------------------------------------------------------------------------
+plot(Bleed.ld,my.title="All Bases")
+
+plot(Bleed.ld,
+     distribution = "Weibull",
+     my.title = "Bleed System Failures\nAll Bases")
+
+plot(Bleed.ld_D,
+     distribution = "Weibull",
+     my.title = "Bleed System Failures\nOnly Base D")
+
+plot(Bleed.ld_Other,
+     distribution = "Weibull",
+     my.title = "Bleed System Failures\nOmitting Base D")
+
+## ------------------------------------------------------------------------
+probpaper("Weibull",
+          xlim = c(1, 10), 
+          grid = TRUE, 
+          ylim = c(0.011,0.981))
+
+probpaper("Weibull",
+          xlim = c(1, 100),
+          grid = TRUE,
+          ylim = c(0.011,0.981))
+
+probpaper("Weibull",
+          xlim = c(1, 1000),
+          grid = TRUE,
+          ylim = c(0.011,0.981))
+
+probpaper("Weibull",
+          xlim = c(1, 1000),
+          grid = TRUE, 
+          ylim = c(0.0011,0.9981))
+
+probpaper("Lognormal",
+          xlim = c(1, 10),
+          grid = TRUE,
+          ylim = c(0.011,0.981))
+
+probpaper("Lognormal",
+          xlim = c(1, 100),
+          grid = TRUE,
+          ylim = c(0.011,0.981))
+
+probpaper("Lognormal",
+          xlim = c(1, 1000),
+          grid = TRUE,
+          ylim = c(0.011,0.981))
+
+## ---- eval=FALSE---------------------------------------------------------
+#  lzbearing.ld <- frame.to.ld(lzbearing, response.column = 1)
+#  plot(lzbearing.ld,
+#       distribution = "gng",
+#       shape = .1,
+#       my.title = "gamma = .1",
+#       linear.axes = "q")
+#  
+#  plot(lzbearing.ld,
+#       distribution = "gng",
+#       shape = .1)
+#  
+#  multiple.probplot.sim()
+#  
+#  multiple.probplot.sim(dist="exponential")
+
