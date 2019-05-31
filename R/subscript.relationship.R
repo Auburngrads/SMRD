52848f214de908c96104f46b2da33919a42c8ac1
Auburngrads <auburngrads@live.com>
2019-05-31 17:02:13 -0400

    DONE WITH VIGNETTES!!!!

--- R/subscript.relationship.R
+++ R/subscript.relationship.R
@@ -3,11 +3,16 @@ function (relationship, i, allow = F)
 {
     funny.sub <- ifelse(is.character(i), !is.element(i, names(relationship)),
         i > length(relationship))
-    if (length(i) > 1 || i == 0 || funny.sub)
+    if (length(i) > 1 || i == 0 || funny.sub){
+        
         stop(paste(c("illegal subscript", i, "for relationship",
             paste(relationship, collapse = ","))))
+        
+    }
     if (i > 0) {
+        
         return.relationship <- multiple.generic.relationship.name(relationship)[i]
+        
         if (any(is.element(multiple.generic.relationship.name(relationship),
             c("Box-Cox", "Eyring")))) {
             if (is.element("the.power", names(attributes(relationship))))
@@ -17,13 +22,16 @@ function (relationship, i, allow = F)
                 stop(c("No power attribute on relationship",
                   paste(relationship, collapse = ",")))
         }
-        if (any(is.element(return.relationship, c("Arrhenius",
-            "Arrhenius3", "Eyring")))) {
+        if (any(is.element(return.relationship, c("Arrhenius", "Arrhenius3", "Eyring")))) {
+            
             attr(return.relationship, "the.arrhenius.units") <- attr(relationship,
                 "the.arrhenius.units")
+            
         }
     }
+    
     if (i < 0) {
+        
         return.relationship <- relationship[i]
         if (any(is.element(multiple.generic.relationship.name(relationship),
             c("Box-Cox", "Eyring")))) {
