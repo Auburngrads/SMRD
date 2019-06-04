199b2877c5bd334ad24189d3ce18d334aec21205
Auburngrads <auburngrads@live.com>
2019-06-04 16:19:36 -0400

    renamed package SMRD and migrated over code

--- vignettes/echapter2.R
+++
@@ -1,9 +0,0 @@
-## ---- echo=FALSE---------------------------------------------------------
-SMRD2:::vinny()
-library(SMRD2)
-
-## ---- fig.width=7, fig.height=5------------------------------------------
-distribution.plot("Weibull",
-                  shape = c(1.7),
-                  prob.range = c(.000001,.99))
-
