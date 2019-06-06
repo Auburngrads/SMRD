20aa182b1d5b262f4d9304077ac06728631951d7
Auburngrads <auburngrads@live.com>
2019-06-06 15:36:06 -0400

    fix cpp tests to work with smrdfortran

--- inst/other_stuff/cpp_tests/MLMOD1_test.R
+++ inst/other_stuff/cpp_tests/MLMOD1_test.R
@@ -1,16 +1,16 @@
-library(SMRD)
+library(smrdfortran)
 times = rexp(10, 0.02)
 a2.init = 1
 a2.limit = 200
 rate = 0.5
 rate.factor = 2
 number.times <- length(times)
-rate.factor <- SMRD:::expand.vec(rate.factor, number.times)
+rate.factor <- smrdfortran:::expand.vec(rate.factor, number.times)
 old <- .Fortran("mlmod1", as.double(times), as.integer(number.times), 
                  as.double(a2.init), as.double(a2.limit), as.double(rate), 
                  as.double(rate.factor), a2 = double(number.times))
 
-new <- SMRD2::MLMOD1(as.double(times), 
+new <- SMRD::MLMOD1(as.double(times), 
                      as.integer(number.times), 
                      as.double(a2.init), 
                      as.double(a2.limit),
