8802cabbd93464495c772483fd471c53ba90dab7
Auburngrads <auburngrads@live.com>
2019-06-06 15:36:37 -0400

    fix cpp tests to work with smrdfortran

--- inst/other_stuff/cpp_tests/SFMCF_test.R
+++ inst/other_stuff/cpp_tests/SFMCF_test.R
@@ -1,12 +1,12 @@
 times <- 1:10
 theta <- c(2,4)
 form <- 'loglinear'
-old <- SMRD:::NHPP.MCFvec(times, theta, form)
+old <- smrdfortran:::NHPP.MCFvec(times, theta, form)
 
 time = as.double(times)
-kform = as.integer(SMRD:::num.nhpp.form(SMRD:::generic.nhpp.form(form)[[1]]))
+kform = as.integer(smrdfortran:::num.nhpp.form(smrdfortran:::generic.nhpp.form(form)[[1]]))
 theta = as.double(theta)
 ntimes = as.integer(length(times))
 answer = double(length(times))
 
-new <- SMRD2::SFMCF(time, kform, theta,ntimes,answer)
+new <- SMRD::SFMCF(time, kform, theta,ntimes,answer)
