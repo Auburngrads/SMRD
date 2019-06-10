b23035f28b11917e9beedff39d6c321cc1db6567
Auburngrads <auburngrads@live.com>
2019-06-10 15:40:31 -0400

    going public

--- R/echapter.R
+++ R/echapter.R
@@ -8,6 +8,20 @@
 #' @export
 echapter <- function(chapter){
   
+  echapters = dir(system.file("echapters", package = "SMRD"), 
+                  pattern = ".Rmd")
+  
+  echaps = gsub(".Rmd","", echapters)
+  
+  e_order = order(as.numeric(gsub("echapter","",echaps)))
+  
+  if(!isTRUE(paste0("echapter",as.numeric(chapter),".Rmd") %in% echapters)){
+    
+     stop("\n\nechapter",chapter,".Rmd does not exist -- Try one of the following:\n\n",
+          paste(echapters[e_order], collapse = "\n"))
+    
+  }
+  
   browseURL(rmarkdown::render(system.file("echapters",
                                           paste0("echapter",as.numeric(chapter),".Rmd"),
                                           package = "SMRD")))
