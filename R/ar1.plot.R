3199c02a7a984cdab34f4a5f62b7d491f9e72c7d
Auburngrads <auburngrads@live.com>
2019-06-11 16:38:23 -0400

    updated plotting functions called by renewal.plots

--- R/ar1.plot.R
+++ R/ar1.plot.R
@@ -1,5 +1,7 @@
 #' Title
 #'
+#' @importFrom stats acf
+#'
 #' @param data.rdu 
 #' @param my.title 
 #' @param xlab 
@@ -33,26 +35,37 @@
 #' 
 #' }
 ar1.plot <-
-function (data.rdu, my.title = NULL, xlab = "Interarrival Times", 
-    ylab = "Lagged Interarrival Times", nacf = 5, plot.acf = F, 
-    ...) 
+function (data.rdu, 
+          my.title = NULL, 
+          xlab = "Interarrival Times", 
+          ylab = "Lagged Interarrival Times", 
+          nacf = 5, 
+          plot.acf = F,...) 
 {
     old.par <- par(mar = c(5.1, 6.1, 4.1, 2.1), pty = "s")
+    
     on.exit({
         par(old.par)
         par(new = F)
     })
+    
     the.interarrival.times <-Response(interarrival.times(data.rdu))
     number.failures <- length(the.interarrival.times)
-    plot(the.interarrival.times[-1], the.interarrival.times[-number.failures], 
-        xlab = "", ylab = "", cex = 1.5, las = 1, ...)
+    plot(the.interarrival.times[-1], 
+         the.interarrival.times[-number.failures],
+         xlab = "", 
+         ylab = "", 
+         cex = 1.5, 
+         las = 1, ...)
+    
     title(xlab = xlab, cex = 1.5)
     title(ylab = ylab, cex = 1.5, mgp = c(4, 1, 0))
-    if (is.null(my.title)) 
-        my.title <- get.data.title(data.rdu)
+    
+    if (is.null(my.title)) my.title <- get.data.title(data.rdu)
     title(my.title)
-    acf.values <- acf(the.interarrival.times, plot = plot.acf)$acf[1:nacf]
-    acf.tvalues <- acf.values * sqrt((length(the.interarrival.times) - 
-        0:(nacf - 1)))
+    
+    acf.values <- stats::acf(the.interarrival.times, plot = plot.acf)$acf[1:nacf]
+    acf.tvalues <- acf.values * sqrt((length(the.interarrival.times) - 0:(nacf - 1)))
+    
     return(list(acf.values = acf.values[-1], acf.tvalues = acf.tvalues[-1]))
 }
