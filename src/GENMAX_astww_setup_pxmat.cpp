124c129fa1d083b34fda2a615c2ab5aaf5c8df7f
Auburngrads <auburngrads@live.com>
2019-07-03 17:04:47 -0400

    Changed printing for xmat

--- src/GENMAX_astww_setup_pxmat.cpp
+++ src/GENMAX_astww_setup_pxmat.cpp
@@ -2,14 +2,9 @@
 
 void pxmat(Rcpp::NumericMatrix x){
   
-for(int i = 0; i < x.nrow(); i++){
-  
-    Rcpp::Rcout << "\nPXMAT\n"                 << std::endl;
-    Rcpp::Rcout << "i = "         << i         << std::endl;
-    Rcpp::Rcout << "xmat(i,0) = " << x.at(i,0) << std::endl;
+    Rcpp::Rcout << "\nPXMAT\n"      << std::endl;
+    Rcpp::Rcout << "xmat = \n" << x << std::endl;
     
-}
-  
 return;
 
 }
