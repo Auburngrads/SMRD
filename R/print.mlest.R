4fac147445aa3ceb0d5f45abf050cc016b1d9959
Auburngrads <auburngrads@live.com>
2019-08-15 00:59:38 -0400

    Update R/print.mlest.R

--- R/print.mlest.R
+++ R/print.mlest.R
@@ -1,15 +1,15 @@
 #' @export
 print.mlest <-
-function (x, 
+function (x,
           conf.level = GetSMRDDefault("SMRD.ConfLevel")/100,
-          digits = GetSMRDDefault("SMRD.DigitsPrinted"), 
+          digits = GetSMRDDefault("SMRD.DigitsPrinted"),
           print.vcv = GetSMRDDefault("SMRD.LongPrint"),
-          add.title = NULL, 
-          quote = T, 
+          add.title = NULL,
+          quote = T,
           prefix = "",
           printem = T,...)
 {
-   mlest.out <- x
+    mlest.out <- x
     variable.names <- period.to.paren(dimnames(mlest.out$vcv.matrix)[[1]])
     dimnames(mlest.out$vcv.matrix) <- list(variable.names, variable.names)
     names(mlest.out$theta.hat) <- variable.names
@@ -31,70 +31,103 @@ function (x,
         if (is.null(group.var)) group.var <- mlest.out$group.var
         the.names <- dimnames(xmat(mlest.out$data.ld)[, group.var,  drop = F])[[2]]
         relat.names <- name.relationship(mlest.out$relationship)
-        arrhenius.names <- ifelse(relat.names == "Arrhenius",
-                                  SMRDOptions("SMRD.Boltzmann"), "")
-        the.table <- matrix(paste(the.names, paste(relat.names, arrhenius.names), sep = ": "), ncol = 1)
+        arrhenius.names <- `if`(relat.names == "Arrhenius",
+                                SMRDOptions("SMRD.Boltzmann"), 
+                                "")
+        
+        the.table <- matrix(paste(the.names, 
+                                  paste(relat.names, arrhenius.names), 
+                                  sep = ": "), 
+                            ncol = 1)
         dimnames(the.table) <- list(1:nrow(the.table), "")
         cat("Variable: Relationship (g)")
         print(the.table, quote = F)
         contrast.method <- attr(mlest.out$the.model.matrix, "contrast.method")
+        
         if (any(multiple.generic.relationship.name(mlest.out$relationship) ==
-            "class") && !is.null(contrast.method))
+            "class") && !is.null(contrast.method)) {
+          
             cat("\nS-PLUS dummy variable coding method used for Class variable(s):\n",
                 paste(contrast.method, collapse = ","), "\n")
+          
+        }
         DecodeDummyVariables(mlest.out)
-        cat("\nModel formula:\n")
-
-        print(deparse(mlest.out[[1]]$terms))
-        cat("\n")
+        
+        cat("\nModel formula:\n") ; print(deparse(mlest.out$terms)) ; cat("\n")
     }
+    
     LL.result<-paste("The maximum value of the log-likelihood function for the",
-              get.data.title(mlest.out$data), add.title, "(assuming the", mlest.out$distribution,
-"distribution is correct) was found to be", format(mlest.out$log.likelihood), sep = " ")
+                     get.data.title(mlest.out$data), 
+                     add.title, 
+                     "(assuming the", 
+                     mlest.out$distribution,
+                     "distribution is correct) was found to be", 
+                     format(mlest.out$log.likelihood), sep = " ")
     param <- mlest.out$theta.hat
     se <- sqrt(diag(mlest.out$vcv.matrix))
     kodet <- mlest.out$kodet
     hold.names <- names(param)
+    
     if (generic.distribution(mlest.out$distribution) == "weibull") {
+      
         if (length(param) == 2 && hold.names[1] == "mu") {
             param <- c(param, exp(param[1]), 1/param[2])
             se <- c(se, se[1] * param[3], se[2]/param[2]^2)
             kodet <- c(kodet, 2, 2)
             names(param) <- c(hold.names, "Weibull (eta)", "Weibull (beta)")
+            
       } else {
+        
             if (kodet[length(param)] == 2) {
+              
                 se <- c(se, se[length(param)]/param[length(param)]^2)
                 param <- c(param, beta = 1/param[length(param)])
                 kodet <- c(kodet, 2)
                 names(param) <- c(hold.names, "Weibull (beta)")
+                
             }
-        }
+        
+      }
+      
     }
+    
     if (generic.distribution(mlest.out$distribution) == "exponential") {
+      
         if (length(param) == 2 && hold.names[1] == "mu") {
+          
             param <- c(param, exp(param[1]))
             se <- c(se, se[1] * param[3])
             kodet <- c(kodet, 2, 2)
             names(param) <- c(hold.names, "Exponential (mean)")
+            
         }
+      
     }
+    
     zquant <- qnorm(1 - (1 - conf.level)/2)
     lower <- param
     upper <- param
+    
     for (i in 1:length(param)) {
+      
         switch(kodet[i], {
             lower[i] <- param[i] - zquant * se[i]
             upper[i] <- param[i] + zquant * se[i]
         }, {
-            lower[i] <- param[i]/exp((zquant * se[i])/param[i])
+            lower[i] <- param[i] / exp((zquant * se[i])/param[i])
             upper[i] <- param[i] * exp((zquant * se[i])/param[i])
         }, )
+      
     }
+    
     esti.matrix <- cbind(param, se, lower, upper)
     conf.char <- percent.conf.level(conf.level)
     indent <- max(nchar(dimnames(esti.matrix)[[1]]))
-    dimnames(esti.matrix)[2] <- list(c("MLE", "Std.Err.", paste(conf.char,
-        "Lower"), paste(conf.char, "Upper")))
+    dimnames(esti.matrix)[2] <- list(c("MLE", 
+                                       "Std.Err.", 
+                                       paste(conf.char,"Lower"), 
+                                       paste(conf.char, "Upper")))
+    
     skip.space <- paste(rep(" ", indent + 7), collapse = "")
 
     mttf.text <- mttf(mlest.out)
