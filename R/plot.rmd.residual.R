37ebcbc1d107e5dd7e3bc23c61b0320993e5331d
Auburngrads <auburngrads@live.com>
2019-08-16 16:21:18 -0400

    general code cleanup

--- R/plot.rmd.residual.R
+++ R/plot.rmd.residual.R
@@ -1,13 +1,22 @@
 #' @export
 plot.rmd.residual <-
-function (x, title.option = GetSMRDDefault("SMRD.TitleOption"), xlab = NULL, ylab = "Residuals",
-    outer.plot = F, order.groups = F, group.var = 1:length(get.x.columns(x)),...)
+function (x, 
+          title.option = GetSMRDDefault("SMRD.TitleOption"), 
+          xlab = NULL, 
+          ylab = "Residuals",
+          outer.plot = F, 
+          order.groups = F, 
+          group.var = 1:length(get.x.columns(x)),...)
 {
     data.rmd <- attr(x, "residual.rmd")
-    if (is.null(data.rmd))
-        stop("Residuals not found---perhpas all units were real failures.")
+    
+    if (is.null(data.rmd)) stop("Residuals not found---perhpas all units were real failures.")
+    
     results <- trellis.plot.repeated.measures.data(data.rmd,
-        title.option = title.option, outer.plot = outer.plot,
-        order.groups = order.groups, xlab = xlab)
+                                                   title.option = title.option, 
+                                                   outer.plot = outer.plot,
+                                                   order.groups = order.groups, 
+                                                   xlab = xlab)
+    
     invisible(results)
 }
