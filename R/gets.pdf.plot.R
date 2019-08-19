6082c7fe18503829353e5fd9095b7608c6037e66
Auburngrads <auburngrads@live.com>
2019-08-19 14:16:25 -0400

    update `gets` plotting functions

--- R/gets.pdf.plot.R
+++ R/gets.pdf.plot.R
@@ -12,57 +12,111 @@
 #' 
 #' }
 gets.pdf.plot <-
-function (...) 
+function (distvec = c("sev", "normal", "lev"),
+          sigmavec = c(-.75, 0, .75),
+          zetavec = c(0.5, 1, 2),
+          alphavec = 0,
+          len = 200,
+          lwd = 1.5,
+          cex = 16,
+          plotem = T,...) 
 {
-    old.par <- par(err = -1, mfrow = c(3, 3), oma = c(0, 6, 2, 
-        0))
-    on.exit({
-        par(old.par)
-        par(new = FALSE)
-    })
-    distvec <- c("sev", "normal", "lev")
-    sigmavecchar <- c("-.75", "0", ".75")
-    sigmavec <- as.numeric(sigmavecchar)
-    varzetavec <- c(0.5, 1, 2)
-    labxy <- matrix(as.numeric(c("-0.56", "1.35", "-0.031", ".90", 
-        ".4828", "1.352")), byrow = T, ncol = 2)
-    xdgets <- function(tvec, varzeta, sigma, distribution) {
-        dgets(tvec, 0, sigma, varzeta, distribution)
-    }
-    lower <- matrix(c(-3, -3, -2, -2, -4, -3, -3, -3, -2), byrow = T, 
-        ncol = 3)
-    upper <- matrix(c(2, 3, 3, 3, 4, 2, 2, 3, 3), byrow = T, 
-        ncol = 3)
-    for (j in 1:3) {
-        for (i in 1:3) {
-            tvec <- seq(lower[j, i], upper[j, i], length = 200)
-            matplot(tvec, outer(tvec, varzetavec, xdgets, sigma = sigmavec[i], 
-                distribution = distvec[j]), lty = 1:length(varzetavec), 
-                type = "l", las = 1, cex = 1.2, xlab = "", ylab = "", 
-                ...)
-            if (j == 1) {
-                if (is.postsctiptok()) {
-                  slab <- paste("~f13~.s~f3~. = ", sigmavecchar[i], 
-                    "~", sep = "")
-                  mixed.text(labxy[i, 1], labxy[i, 2], slab, 
-                    adj = 0.5, cex = 1.5)
+    
+    distvec <- tolower(distvec)
+    
+    distvec <- gsub("^[pdqr]?sev[(_|\\s*)]?\\D*", "SEV", distvec)
+    distvec <- gsub("^[pdqr]?lev[(_|\\s*)]?\\D*", "LEV", distvec)
+    distvec <- gsub("^[pdqr]?nor[mal]?[(_|\\s*)]?\\D*", "Normal", distvec)
+    
+    distvec <- distvec[!duplicated(distvec)]
+    
+    if(length(distvec) > 3) stop("Incorrect distributions provided")
+    
+    sigmavec <- as.numeric(sigmavec)
+    
+    DIST  <- rep(distvec, each = len * length(sigmavec) * length(zetavec) * length(alphavec))
+    SIG   <- rep(rep(sigmavec, each = len * length(zetavec) * length(alphavec)), by = length(distvec))
+    ZETA  <- rep(rep(zetavec, each = len * length(alphavec)), by = length(sigmavec) * length(distvec))
+    ALPHA <- rep(rep(alphavec, each = len), by = length(sigmavec) * length(distvec) * length(zetavec))
+    
+    DF <- data.frame(dist = DIST, 
+                     sigma = SIG, 
+                     zeta = ZETA,
+                     alpha = ALPHA,
+                     stringsAsFactors = F)
+
+    lower <- matrix(rep(-4, length.out = length(distvec) * length(sigmavec)), 
+                    byrow = T,
+                    nrow = length(distvec),
+                    ncol = length(sigmavec))
+    
+    upper <- matrix(rep(4, length.out = length(distvec) * length(sigmavec)), 
+                    byrow = T,
+                    nrow = length(distvec),
+                    ncol = length(sigmavec))
+    
+    TVEC <- c()
+    GETS <- c()
+    
+    for(j in 1:length(distvec)) {
+        
+        for(i in 1:length(sigmavec)) {
+          
+            for(k in 1:length(zetavec)) {
+              
+                for(l in 1:length(alphavec)) {
+            
+                    tvec <- seq(lower[j, i], upper[j, i], length = len)
+                    TVEC <- c(TVEC,tvec)
+                    gets <- dgets(tvec,alphavec[l],zetavec[k],sigma = sigmavec[i], distribution = distvec[j])
+                    GETS <- c(GETS,gets)
+                
                 }
-                else {
-                  slab <- paste("s=", sigmavecchar[i], sep = "")
-                  if (is.R()) 
-                    text(labxy[i, 1], labxy[i, 2], slab, cex = 1.5, 
-                      xpd = TRUE)
-                  else text(labxy[i, 1], labxy[i, 2], slab, cex = 1.5)
-                }
+            
             }
+        
         }
+      
     }
-    if (!is.R()) {
-        cexylab <- 1.5
-    }
-    else {
-        cexylab <- 1.2
-    }
-    mtext("LEV                        Normal                      SEV", 
-        side = 2, outer = T, cex = cexylab, line = 1.5)
+
+DF$tvec = TVEC
+DF$gets = GETS
+
+DF$dist[DF[["dist"]] == "Normal"] <- "NORGETS"
+DF$dist[DF[["dist"]] == "SEV"] <- "SEVGETS"
+DF$dist[DF[["dist"]] == "LEV"] <- "LEVGETS"
+
+RET <- list(norgets = subset(DF, "dist" == "NORGETS"),
+            sevgets = subset(DF, "dist" == "SEVGETS"),
+            levgets = subset(DF, "dist" == "LEVGETS"))
+
+DF$params = paste("\u03B6",",","\u03B1"," = ",
+                  DF$zeta,",", DF$alpha, sep = "")
+
+DF$zeta  = as.factor(DF$zeta)
+DF$alpha = as.factor(DF$alpha)
+
+DF$sigma = paste("\u03C3 = ", DF$sigma, sep = "")
+
+colnames(DF) <- c("Distribution","Sigma","Zeta","Alpha","Time","gets","Parameters")
+
+if(!plotem) return(RET)
+    
+  p <- ggplot2::ggplot(DF) +
+         ggplot2::geom_line(ggplot2::aes(x = Time,
+                                         y = gets,
+                                         group    = Parameters, 
+                                         colour   = Parameters,
+                                         linetype = Parameters),
+                            size = lwd) +
+         #theme(line = element_line(size = 20)) +
+         ggplot2::theme_bw(base_size = cex) +
+         ggplot2::facet_grid(Distribution ~ Sigma, scales = 'free') +
+         ggplot2::xlab("Time (t)") +
+         ggplot2::ylab("PDF - f(t|\u03B6,\u03C3,\u03B1)")
+      
+  invisible(RET)
+  
+  print(p)
+  
 }
